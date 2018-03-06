bb739877a6aa9c1dedfbe90e2538964c58b8b22c
evan12 <574583177@qq.com>
2018-03-06 23:37:52 +0800

    修改时间戳字段可为空

--- database/migrations/2018_03_06_222836_create_orders_table.php
+++ database/migrations/2018_03_06_222836_create_orders_table.php
@@ -27,11 +27,11 @@ class CreateOrdersTable extends Migration
             $table->decimal('freight_price')->default(0)->comment('运费');
             // 物流信息 -> 放另外的物流信息表中
             $table->tinyInteger('status')->default(0)->comment('状态 1-未支付 2-已支付 3-有退款(聚合父订单存在该状态) 4-全部退款');
-            $table->timestamp('order_time')->default(0)->comment('下单时间');
+            // 下单时间见 created_at 字段
             $table->decimal('pay_price')->default(0)->comment('支付金额');
-            $table->timestamp('pay_time')->default(0)->comment('付款时间');
+            $table->timestamp('pay_time')->nullable()->comment('付款时间');
             $table->decimal('refund_price')->default(0)->comment('退款金额 (聚合父订单为退款总金额)');
-            $table->timestamp('refund_time')->default(0)->comment('退款时间 (聚合父订单为最后退款时间)');
+            $table->timestamp('refund_time')->nullable()->comment('退款时间 (聚合父订单为最后退款时间)');
             $table->text('snapshoot')->comment('商品快照');
             $table->timestamps();
             $table->softDeletes();
