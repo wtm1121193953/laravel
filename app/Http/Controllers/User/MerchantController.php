53789012dbd0f70e5f44593f3119cfaed24cc1b6
evan12 <574583177@qq.com>
2018-04-15 11:34:57 +0800

    注释掉用户端获取商户时的审核状态限制

--- app/Http/Controllers/User/MerchantController.php
+++ app/Http/Controllers/User/MerchantController.php
@@ -36,7 +36,7 @@ class MerchantController extends Controller
         }
 
         $query = Merchant::where('status', 1)
-            ->where('audit_status', Merchant::AUDIT_STATUS_SUCCESS)
+//            ->where('audit_status', Merchant::AUDIT_STATUS_SUCCESS)
             ->when($city_id, function(Builder $query) use ($city_id){
                 $query->where('city_id', $city_id);
             })
