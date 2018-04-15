c71b1128fce80efe4d8924105edad2506f5b882b
evan12 <574583177@qq.com>
2018-04-15 20:07:47 +0800

    调试微信支付

--- app/Http/Middleware/User/MockMiniprogramEnv.php
+++ app/Http/Middleware/User/MockMiniprogramEnv.php
@@ -41,7 +41,7 @@ class MockMiniprogramEnv
                 'token' => $token
             ]);
             // 绑定token与openId的关联
-            $openid = 'mock_open_id';
+            if(! $openid = $request->get('open_id')) $openid = 'mock_open_id';
             Cache::add('open_id_for_token_' . $token, $openid, 60 * 24 * 30);
 
         }
