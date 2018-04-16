b711cfcc67363e12592972a7bd661bb9d34dea75
evan12 <574583177@qq.com>
2018-04-16 21:49:46 +0800

    下单接口添加该订单是否属于运营中心自己

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -97,26 +97,32 @@ class OrderController extends Controller
         $order->buy_number = $number;
         $order->status = Order::STATUS_UN_PAY;
         $order->pay_price = $goods->price * $number;
+        $order->save();
 
-
-        $payApp = WechatService::getWechatPayAppForOper($merchant->oper_id);
-        $data = [
-            'body' => $order->goods_name,
-            'out_trade_no' => $orderNo,
-            'total_fee' => $order->pay_price * 100,
-            'trade_type' => 'JSAPI',
-            'openid' => $order->open_id,
-        ];
-        $unifyResult = $payApp->order->unify($data);
-        if($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS'){
-            $order->save();
+        $isOperSelf = $merchant->oper_id === $oper->id ? 1 : 0;
+        if($isOperSelf == 1) {
+            $payApp = WechatService::getWechatPayAppForOper($merchant->oper_id);
+            $data = [
+                'body' => $order->goods_name,
+                'out_trade_no' => $orderNo,
+                'total_fee' => $order->pay_price * 100,
+                'trade_type' => 'JSAPI',
+                'openid' => $order->open_id,
+            ];
+            $unifyResult = $payApp->order->unify($data);
+            if($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS'){
+                $order->save();
+            }else {
+                Log::error('微信统一下单失败', [
+                    'payConfig' => $payApp->getConfig(),
+                    'data' => $data,
+                    'result' => $unifyResult,
+                ]);
+                throw new BaseResponseException('微信统一下单失败');
+            }
+            $sdkConfig = $payApp->jssdk->sdkConfig($unifyResult['prepay_id']);
         }else {
-            Log::error('微信统一下单失败', [
-                'payConfig' => $payApp->getConfig(),
-                'data' => $data,
-                'result' => $unifyResult,
-            ]);
-            throw new BaseResponseException('微信统一下单失败');
+            $sdkConfig = null;
         }
 
         if(App::environment() === 'local'){
@@ -136,10 +142,9 @@ class OrderController extends Controller
             $order->save();
         }
 
-        $sdkConfig = $payApp->jssdk->sdkConfig($unifyResult['prepay_id']);
-
         return Result::success([
             'order_no' => $orderNo,
+            'isOperSelf' => $isOperSelf,
             'sdk_config' => $sdkConfig
         ]);
     }
