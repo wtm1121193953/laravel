5d020304a14998268596ab00bab6183d36070e60
evan12 <574583177@qq.com>
2018-04-16 18:56:47 +0800

    修改退款记录表字段默认值

--- database/migrations/2018_04_10_175224_create_order_refunds_table.php
+++ database/migrations/2018_04_10_175224_create_order_refunds_table.php
@@ -18,7 +18,7 @@ class CreateOrderRefundsTable extends Migration
             $table->integer('order_id')->index()->comment('订单ID');
             $table->string('order_no', 100)->index()->comment('订单号');
             $table->decimal('amount')->comment('退款金额');
-            $table->string('refund_id')->index()->comment('微信退款单号');
+            $table->string('refund_id')->default('')->index()->comment('微信退款单号');
             $table->tinyInteger('status')->default(1)->index()->comment('退款状态 1-未退款 2-已退款');
             $table->timestamps();
 
