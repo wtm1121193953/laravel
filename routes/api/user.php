c1794679ff821dc8aa2058039b4415d8d7247848
evan12 <574583177@qq.com>
2018-04-16 11:33:29 +0800

    添加立即付款接口

--- routes/api/user.php
+++ routes/api/user.php
@@ -28,6 +28,7 @@ Route::prefix('user')
         Route::get('orders', 'OrderController@getList')->middleware(UserLoginFilter::class);
         Route::get('order/detail', 'OrderController@detail')->middleware(UserLoginFilter::class);
         Route::any('order/buy', 'OrderController@buy')->middleware(UserLoginFilter::class);
+        Route::any('order/pay', 'OrderController@pay')->middleware(UserLoginFilter::class);
         Route::any('order/refund', 'OrderController@refund')->middleware(UserLoginFilter::class);
 //        Route::get('order/pay', 'OrderController@pay');
 
