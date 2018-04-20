883d1bb1732a86b3fc8fcbc51db6682be993ec18
evan12 <574583177@qq.com>
2018-04-20 09:43:26 +0800

    订单添加商品截止日期

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -50,6 +50,7 @@ class OrderController extends Controller
             $item->items = OrderItem::where('order_id', $item->id)->get();
             // 判断商户是否是当前小程序关联运营中心下的商户
             $item->isOperSelf = $item->oper_id === $currentOperId ? 1 : 0;
+            $item->goods_end_date = Goods::where('id', $item->goods_id)->value('end_date');
         });
         return Result::success([
             'list' => $data->items(),
