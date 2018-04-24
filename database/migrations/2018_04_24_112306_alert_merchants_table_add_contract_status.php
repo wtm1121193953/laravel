d5bcc203b9f9fae90962b82b68999d53e9485057
evan <574583177@qq.com>
2018-04-24 11:39:59 +0800

    商家表添加合同签订状态与录入资料的运营中心ID字段

---
+++ database/migrations/2018_04_24_112306_alert_merchants_table_add_contract_status.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlertMerchantsTableAddContractStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('merchants', function (Blueprint $table) {
+            $table->integer('entry_oper_id')->default(0)->comment('录入资料的运营中心ID');
+            $table->tinyInteger('contract_status')->default(1)->comment('合同签订状态 1-已签订 2-未签订 (未签订合同的商家只有资料, 没有后续操作, 且没有所属运营中心)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('merchants', function (Blueprint $table) {
+            $table->dropColumn(['entry_oper_id', 'contract_status']);
+        });
+    }
+}
