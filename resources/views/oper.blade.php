f253a365acd2e0ac3c2a89c941aff9e5cf2ed3a5
evan12 <574583177@qq.com>
2018-05-06 17:57:49 +0800

    修改日志存储时间, admin,商户及运营中心页面添加webkit浏览器渲染声明

--- resources/views/oper.blade.php
+++ resources/views/oper.blade.php
@@ -2,6 +2,7 @@
 <html lang="zh-CN">
 <head>
     <meta charset="utf-8">
+    <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
