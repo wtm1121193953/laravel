fb1c1bd1b24bdcbc4e4d1d4024930e7b24df5e02
evan12 <574583177@qq.com>
2018-05-11 15:05:32 +0800

    用户端添加根据场景ID获取邀请渠道信息接口

--- routes/api/user.php
+++ routes/api/user.php
@@ -35,7 +35,7 @@ Route::prefix('user')
         Route::any('order/pay', 'OrderController@pay')->middleware(UserLoginFilter::class);
         Route::any('order/refund', 'OrderController@refund')->middleware(UserLoginFilter::class);
 
-        Route::get('invite/qrcode', 'InviteChannel@getInviteQrcode')->middleware(UserLoginFilter::class);
-        Route::get('invite/getInviterBySceneId', 'InviteChannel@getInviterInfo');
+        Route::get('invite/qrcode', 'InviteChannelController@getInviteQrcode')->middleware(UserLoginFilter::class);
+        Route::get('invite/getInviterBySceneId', 'InviteChannelController@getInviterInfo');
 
     });
