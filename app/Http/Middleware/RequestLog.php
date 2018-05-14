9460eb3a7a6795a4b64ae52345dc228e97ef5efb
evan12 <574583177@qq.com>
2018-05-14 18:00:02 +0800

    修改日志记录方式, Response转换为数组

--- app/Http/Middleware/RequestLog.php
+++ app/Http/Middleware/RequestLog.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use App\Support\Utils;
 use Closure;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
 class RequestLog
@@ -19,6 +19,7 @@ class RequestLog
      */
     public function handle($request, Closure $next)
     {
+        /** @var \Symfony\Component\HttpFoundation\Response|\Illuminate\Contracts\Routing\ResponseFactory $response */
         $response = $next($request);
         $attributes = $request->attributes->all();
         foreach ($attributes as $key => $attribute) {
@@ -26,10 +27,22 @@ class RequestLog
                 $attributes[$key] = $attribute->toArray();
             }
         }
-        Log::info('request listen ', [
+        $logData = [
             'request' => Utils::getRequestContext($request),
-            'response' => $response,
-        ]);
+            'response' => [
+                'statusCode' => $response->getStatusCode(),
+                'content' => 'content is not json'
+            ],
+        ];
+        if($response instanceof JsonResponse){
+            $data = $response->getContent();
+            if(is_string($data)){
+                $data = json_decode($data, 1);
+            }
+            $logData['response']['content'] = $data;
+        }
+
+        Log::info('request listen ', $logData);
         return $response;
     }
 }
