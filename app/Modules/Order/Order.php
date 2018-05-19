875fd0977b8daecb0c158047d52b467c66485372
evan12 <574583177@qq.com>
2018-05-19 13:52:30 +0800

    订单模型添加订单类型,支付类型,支付目标类型以及订单来源客户端类型常量

--- app/Modules/Order/Order.php
+++ app/Modules/Order/Order.php
@@ -7,14 +7,42 @@ use App\Exceptions\BaseResponseException;
 
 class Order extends BaseModel
 {
-    //
-    const STATUS_UN_PAY = 1;
-    const STATUS_CANCEL = 2;
-    const STATUS_CLOSED = 3;
-    const STATUS_PAID = 4;
-    const STATUS_REFUNDING = 5;
-    const STATUS_REFUNDED = 6;
-    const STATUS_FINISHED = 7;
+    /**
+     * 订单类型
+     */
+    const TYPE_GROUP_BUY = 1; // 团购订单
+    const TYPE_SCAN_QRCODE_PAY = 2; // 扫码支付订单, 即直接输入金额支付
+    const TYPE_DISHES = 3; // 点菜订单
+
+    /**
+     * 支付类型
+     */
+    const PAY_TYPE_WECHAT = 1; // 微信支付
+    const PAY_TYPE_ALIPAY = 2; // 支付宝支付
+
+    /**
+     * 支付目标类型
+     */
+    const PAY_TARGET_TYPE_OPER = 1; // 支付给运营中心
+    const PAY_TARGET_TYPE_PLATFORM = 2; // 支付给平台
+
+    /**
+     * 订单来源客户端app类型
+     */
+    const ORIGIN_APP_TYPE_ANDROID = 1; // 安卓
+    const ORIGIN_APP_TYPE_IOS = 2; // iOS
+    const ORIGIN_APP_TYPE_MINIPROGRAM = 3; // 小程序
+
+    /**
+     * 订单状态
+     */
+    const STATUS_UN_PAY = 1; // 未支付
+    const STATUS_CANCEL = 2; // 已取消
+    const STATUS_CLOSED = 3; // 已关闭
+    const STATUS_PAID = 4; // 已支付
+    const STATUS_REFUNDING = 5; // 退款中
+    const STATUS_REFUNDED = 6; // 已退款
+    const STATUS_FINISHED = 7; // 已完成
 
     /**
      * 生成订单号, 订单号规则: O{年月日时分秒}{6位随机数}
