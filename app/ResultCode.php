d30ee1c01894dc6f6911d885a0c8eca1c28fd57a
evan12 <574583177@qq.com>
2018-05-24 14:22:34 +0800

    优化错误信息返回

--- app/ResultCode.php
+++ app/ResultCode.php
@@ -56,4 +56,5 @@ class ResultCode
     const WECHAT_OPEN_ID_NOT_FOUND = 70010; // 微信openId不存在
     const WECHAT_APPID_INVALID = 70011; // 微信appid不存在
     const MINIPROGRAM_PAGE_NOT_EXIST = 70020; // 微信小程序页面不存在
+    const MINIPROGRAM_CONFIG_NOT_EXIST = 70021; // 微信小程序配置不存在
 }
