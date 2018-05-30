06641a8448757fb7f2260fa078f8bdfaaea7244d
evan12 <574583177@qq.com>
2018-05-30 16:44:42 +0800

    添加用户积分服务方法

--- app/Modules/UserCredit/UserCreditSettingService.php
+++ app/Modules/UserCredit/UserCreditSettingService.php
@@ -14,4 +14,73 @@ use App\Modules\Setting\SettingService;
 class UserCreditSettingService extends SettingService
 {
 
+    /**
+     * 获取直推用户累计额度换算系数
+     */
+    public static function getConsumeQuotaConvertRatioSetting()
+    {
+        return self::getValueByKey('consume_quota_convert_ratio_to_parent');
+    }
+
+    /**
+     * 获取金额与积分转换比例
+     */
+    public static function getCreditMultiplierOfAmountSetting()
+    {
+        return self::getValueByKey('credit_multiplier_of_amount');
+    }
+
+    /**
+     * 获取运营中心抽成比例配置
+     */
+    public static function getOperProfitRadioSetting()
+    {
+        return self::getValueByKey('oper_profit_radio');
+    }
+
+    /**
+     * 根据用户积分数量获取用户等级
+     * @param int $creditNumber
+     */
+    public static function getUserLevelByCreditNumber($creditNumber)
+    {
+        // todo
+    }
+
+    /**
+     * 根据商户邀请的用户数量获取商户等级
+     * @param int $inviteUserNumber
+     */
+    public static function getMerchantLevelByInviteUserNumber($inviteUserNumber)
+    {
+        // todo
+    }
+
+    /**
+     * 根据用户等级获取自返比例配置
+     * @param $userLevel
+     */
+    public static function getCreditToSelfRatioSetting($userLevel)
+    {
+        // todo
+    }
+
+    /**
+     * 根据用户等级获取 分享提成 比例
+     * @param $userLevel
+     */
+    public static function getCreditToParentRatioSetting($userLevel)
+    {
+        // todo
+    }
+
+    /**
+     * 根据商户等级获取 商户等级加成 配置
+     * @param $merchantLevel
+     */
+    public static function getCreditMultiplierOfMerchantSetting($merchantLevel)
+    {
+        // todo
+    }
+
 }
