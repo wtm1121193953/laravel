efcf1eb4f43b98298ed422760e2df81d5512f92d
evan12 <574583177@qq.com>
2018-05-31 21:55:10 +0800

    订单号添加索引

--- database/migrations/2018_05_30_145614_create_user_credit_records_table.php
+++ database/migrations/2018_05_30_145614_create_user_credit_records_table.php
@@ -21,7 +21,7 @@ class CreateUserCreditRecordsTable extends Migration
             $table->tinyInteger('type')->index()->default(1)->comment('来源类型(1-自返 2-分享提成 3-商家提成 4-退款退回 5-消费支出)');
             $table->tinyInteger('user_level')->default(1)->comment('产生积分时的用户等级');
             $table->tinyInteger('merchant_level')->default(1)->comment('积分产生时的商家等级(类型为商家提成时存在)');
-            $table->string('order_no')->default('')->comment('关联订单号(类型为消费支出时为积分订单号, 其他为正常的订单号)');
+            $table->string('order_no', 50)->index()->default('')->comment('关联订单号(类型为消费支出时为积分订单号, 其他为正常的订单号)');
             $table->string('consume_user_mobile', 11)->default('')->comment('消费用户手机号');
             $table->decimal('order_profit_amount')->default(0)->comment('订单利润');
             $table->decimal('ratio', 4, 2)->default(0)->comment('返利比例[=用户等级对应比例 * 用户所关联的商户等级加成(若存在), 是一个百分比, 如20则表示百分之20]');
