c33b577ef212e8ddabfe4b5e6291ef343862292f
evan12 <574583177@qq.com>
2018-06-03 16:34:39 +0800

    修改订单支付成功时的状态变更
    (cherry picked from commit 188e4a9)

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -88,8 +88,15 @@ class PayController extends Controller
             || $order->status === Order::STATUS_CLOSED
         ){
             $order->pay_time = Carbon::now(); // 更新支付时间为当前时间
-            $order->status = Order::STATUS_PAID;
-            $order->save();
+            if($order->type == Order::TYPE_SCAN_QRCODE_PAY){
+                // 如果是扫码付款, 直接改变订单状态为已完成
+                $order->status = Order::STATUS_FINISHED;
+                $order->finish_time = Carbon::now();
+                $order->save();
+            }else {
+                $order->status = Order::STATUS_PAID;
+                $order->save();
+            }
 
             // 添加商品已售数量
             Goods::where('id', $order->goods_id)->increment('sell_number');
