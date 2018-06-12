ac30d1dcc87bebe3d745cb94c8a651aa0d0f8def
evan.lee <evan.li@shoptps.com>
2018-06-12 14:30:03 +0800

    添加用户邀请解绑记录表

---
+++ app/Http/Controllers/User/UnbindInviterController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evan.li
+ * Date: 2018/6/12
+ * Time: 12:14
+ */
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * 解除绑定关系控制器
+ * Class UnbindInviterController
+ * @package App\Http\Controllers
+ */
+class UnbindInviterController extends Controller
+{
+
+    /**
+     * 获取已绑定的细信息
+     */
+    public function getBindInfo()
+    {
+
+    }
+}
