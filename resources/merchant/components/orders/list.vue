a6632d10e6d8beedc7827812c82e12b045b703a4
xianghua <zengxianghua5@163.com>
2018-06-26 10:34:05 +0800

    点菜换单品

--- resources/merchant/components/orders/list.vue
+++ resources/merchant/components/orders/list.vue
@@ -22,7 +22,7 @@
                 <template slot-scope="scope">
                     <span v-if="scope.row.type == 1">团购</span>
                     <span v-else-if="scope.row.type == 2">买单</span>
-                    <span v-else-if="scope.row.type == 3">点菜</span>
+                    <span v-else-if="scope.row.type == 3">单品</span>
                     <span v-else>未知({{scope.row.type}})</span>
                 </template>
             </el-table-column>
