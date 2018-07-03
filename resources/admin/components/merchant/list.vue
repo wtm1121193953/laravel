deb12c8d5ba3d43141512eb755ecf93956e647d9
liwen <wen.li@shoptps.com>
2018-07-03 15:29:29 +0800

    去掉一些注释

--- resources/admin/components/merchant/list.vue
+++ resources/admin/components/merchant/list.vue
@@ -1,155 +1,148 @@
 <template>
     <page title="商户审核管理" v-loading="isLoading" >
-        <!--<el-tabs v-model="activeTab" type="card" @tab-click="changeTab">-->
-            <!--<el-tab-pane label="待审核商户列表" name="merchant">-->
 
-                <el-col>
-                    <el-form v-model="query" inline>
-                        <el-form-item prop="merchantId" label="商户ID">
-                            <el-input v-model="query.merchantId" size="small" class="w-100" clearable></el-input>
-                        </el-form-item>
-                        <el-form-item prop="name" label="商户名称">
-                            <el-input v-model="query.name" size="small" placeholder="商户名称" @keyup.enter.native="search"/>
-                        </el-form-item>
-                        <el-form-item label="审核状态" prop="auditStatus"  v-if="isAudit">
-                            <el-select v-model="query.auditStatus" size="small" multiple  placeholder="请选择" class="w-150">
-                                <el-option label="待审核" value="0" />
-                                <el-option label="重新提交审核" value="3"/>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="审核状态" prop="auditStatus"  v-else>
-                            <el-select v-model="query.auditStatus" size="small"  multiple placeholder="请选择" class="w-150">
-                                <el-option label="全部" value="" />
-                                <el-option label="待审核" value="0"/>
-                                <el-option label="审核通过" value="1"/>
-                                <el-option label="审核不通过" value="2"/>
-                                <el-option label="重新提交审核" value="3"/>
-                            </el-select>
-                        </el-form-item>
+        <el-col>
+            <el-form v-model="query" inline>
+                <el-form-item prop="merchantId" label="商户ID">
+                    <el-input v-model="query.merchantId" size="small" class="w-100" clearable></el-input>
+                </el-form-item>
+                <el-form-item prop="name" label="商户名称">
+                    <el-input v-model="query.name" size="small" placeholder="商户名称" @keyup.enter.native="search"/>
+                </el-form-item>
+                <el-form-item label="审核状态" prop="auditStatus"  v-if="isAudit">
+                    <el-select v-model="query.auditStatus" size="small" multiple  placeholder="请选择" class="w-150">
+                        <el-option label="待审核" value="0" />
+                        <el-option label="重新提交审核" value="3"/>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="审核状态" prop="auditStatus"  v-else>
+                    <el-select v-model="query.auditStatus" size="small"  multiple placeholder="请选择" class="w-150">
+                        <el-option label="全部" value="" />
+                        <el-option label="待审核" value="0"/>
+                        <el-option label="审核通过" value="1"/>
+                        <el-option label="审核不通过" value="2"/>
+                        <el-option label="重新提交审核" value="3"/>
+                    </el-select>
+                </el-form-item>
 
 
-                        <el-form-item prop="startDate" label="添加商户开始时间">
-                            <el-date-picker
-                                v-model="query.startDate"
-                                type="date"
-                                size="small"
-                                placeholder="选择开始日期"
-                                format="yyyy 年 MM 月 dd 日"
-                                value-format="yyyy-MM-dd"
-                            ></el-date-picker>
-                        </el-form-item>
-                        <el-form-item prop="startDate" label="结束时间">
-                            <el-date-picker
-                                v-model="query.endDate"
-                                type="date"
-                                size="small"
-                                placeholder="选择结束日期"
-                                format="yyyy 年 MM 月 dd 日"
-                                value-format="yyyy-MM-dd"
-                                :picker-options="{disabledDate: (time) => {return time.getTime() < new Date(query.startDate) - 8.64e7}}"
-                            ></el-date-picker>
-                        </el-form-item>
+                <el-form-item prop="startDate" label="添加商户开始时间">
+                    <el-date-picker
+                        v-model="query.startDate"
+                        type="date"
+                        size="small"
+                        placeholder="选择开始日期"
+                        format="yyyy 年 MM 月 dd 日"
+                        value-format="yyyy-MM-dd"
+                    ></el-date-picker>
+                </el-form-item>
+                <el-form-item prop="startDate" label="结束时间">
+                    <el-date-picker
+                        v-model="query.endDate"
+                        type="date"
+                        size="small"
+                        placeholder="选择结束日期"
+                        format="yyyy 年 MM 月 dd 日"
+                        value-format="yyyy-MM-dd"
+                        :picker-options="{disabledDate: (time) => {return time.getTime() < new Date(query.startDate) - 8.64e7}}"
+                    ></el-date-picker>
+                </el-form-item>
 
-                        <el-form-item prop="operName" label="激活运营中心名称">
-                            <el-input v-model="query.operName" size="small"  clearable></el-input>
-                        </el-form-item>
-                        <el-form-item prop="operId" label="激活运营中心ID">
-                            <el-input v-model="query.operId" size="small" />
-                        </el-form-item>
-                        <el-form-item prop="creatorOperName" label="录入运营中心名称">
-                            <el-input v-model="query.creatorOperName" size="small" clearable></el-input>
-                        </el-form-item>
-                        <el-form-item prop="creatorOperId" label="录入运营中心ID">
-                            <el-input v-model="query.creatorOperId" size="small"  />
-                        </el-form-item>
+                <el-form-item prop="operName" label="激活运营中心名称">
+                    <el-input v-model="query.operName" size="small"  clearable></el-input>
+                </el-form-item>
+                <el-form-item prop="operId" label="激活运营中心ID">
+                    <el-input v-model="query.operId" size="small" />
+                </el-form-item>
+                <el-form-item prop="creatorOperName" label="录入运营中心名称">
+                    <el-input v-model="query.creatorOperName" size="small" clearable></el-input>
+                </el-form-item>
+                <el-form-item prop="creatorOperId" label="录入运营中心ID">
+                    <el-input v-model="query.creatorOperId" size="small"  />
+                </el-form-item>
 
-                        <el-form-item>
-                            <el-button type="primary" size="small" @click="search"><i class="el-icon-search">搜 索</i></el-button>
-                        </el-form-item>
-                        <el-form-item class="fr">
-                            <el-button type="success" size="small" @click="downloadExcel">导出Excel</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-col>
+                <el-form-item>
+                    <el-button type="primary" size="small" @click="search"><i class="el-icon-search">搜 索</i></el-button>
+                </el-form-item>
+                <el-form-item class="fr">
+                    <el-button type="success" size="small" @click="downloadExcel">导出Excel</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
 
-                <el-table :data="list" v-loading="tableLoading" stripe>
-                    <el-table-column prop="created_at" label="添加时间"/>
-                    <el-table-column prop="id" label="商户ID"/>
-                    <el-table-column prop="name" label="商户名称"/>
-                    <el-table-column prop="operID" label="激活运营中心ID"/>
-                    <el-table-column prop="operName" label="激活运营中心名称"/>
-                    <el-table-column prop="creatorOperId" label="录入运营中心ID"/>
-                    <el-table-column prop="creatorOperName" label="录入运营中心名称"/>
-                    <el-table-column prop="categoryPath" label="行业">
-                        <template slot-scope="scope">
-                    <span v-for="item in scope.row.categoryPath" :key="item.id">
-                        {{ item.name }}
-                    </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="city" label="城市">
-                        <template slot-scope="scope">
-                            <!--<span> {{ scope.row.province }} </span>-->
-                            <span> {{ scope.row.city }} </span>
-                            <span> {{ scope.row.area }} </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="audit_status" label="审核状态">
-                        <template slot-scope="scope">
-                            <span v-if="scope.row.audit_status === 0" class="c-warning">待审核</span>
-                            <span v-else-if="scope.row.audit_status === 1" class="c-green">审核通过</span>
-                            <el-popover v-else-if="scope.row.audit_status === 2"  placement="bottom"  title="标题"  width="200"  trigger="hover"
-                                        show="showMessage">
-                                <!--<el-table :data="gridData">-->
-                                    <!--<el-table-column width="150" property="date" label="日期"></el-table-column>-->
-                                    <!--<el-table-column width="100" property="name" label="姓名"></el-table-column>-->
-                                    <!--<el-table-column width="300" property="address" label="地址"></el-table-column>-->
-                                <!--</el-table>-->
-                                <span   slot="reference" class="c-danger">审核不通过</span>
-                            </el-popover>
+        <el-table :data="list" v-loading="tableLoading" stripe>
+            <el-table-column prop="created_at" label="添加时间"/>
+            <el-table-column prop="id" label="商户ID"/>
+            <el-table-column prop="name" label="商户名称"/>
+            <el-table-column prop="operID" label="激活运营中心ID"/>
+            <el-table-column prop="operName" label="激活运营中心名称"/>
+            <el-table-column prop="creatorOperId" label="录入运营中心ID"/>
+            <el-table-column prop="creatorOperName" label="录入运营中心名称"/>
+            <el-table-column prop="categoryPath" label="行业">
+                <template slot-scope="scope">
+            <span v-for="item in scope.row.categoryPath" :key="item.id">
+                {{ item.name }}
+            </span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="city" label="城市">
+                <template slot-scope="scope">
+                    <!--<span> {{ scope.row.province }} </span>-->
+                    <span> {{ scope.row.city }} </span>
+                    <span> {{ scope.row.area }} </span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="audit_status" label="审核状态">
+                <template slot-scope="scope">
+                    <span v-if="scope.row.audit_status === 0" class="c-warning">待审核</span>
+                    <span v-else-if="scope.row.audit_status === 1" class="c-green">审核通过</span>
+                    <el-popover v-else-if="scope.row.audit_status === 2"  placement="bottom"  title="标题"  width="200"  trigger="hover"
+                                show="showMessage">
+                        <!--<el-table :data="gridData">-->
+                            <!--<el-table-column width="150" property="date" label="日期"></el-table-column>-->
+                            <!--<el-table-column width="100" property="name" label="姓名"></el-table-column>-->
+                            <!--<el-table-column width="300" property="address" label="地址"></el-table-column>-->
+                        <!--</el-table>-->
+                        <span   slot="reference" class="c-danger">审核不通过</span>
+                    </el-popover>
 
 
-                            <span v-else-if="scope.row.audit_status === 3" class="c-warning">待审核(重新提交)</span>
-                            <span v-else>未知 ({{scope.row.audit_status}})</span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="操作" width="150px">
-                        <template slot-scope="scope">
-                            <el-button type="text" @click="detail(scope)">查看</el-button>
+                    <span v-else-if="scope.row.audit_status === 3" class="c-warning">待审核(重新提交)</span>
+                    <span v-else>未知 ({{scope.row.audit_status}})</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="150px">
+                <template slot-scope="scope">
+                    <el-button type="text" @click="detail(scope)">查看</el-button>
 
-                            <template v-if="scope.row.audit_status === 0 || scope.row.audit_status === 3">
-                                <el-button type="text" @click="detail(scope,3)">审核</el-button>
-                                <el-dropdown trigger="click" style="margin-left: 10px;" @command="(command) => {audit(scope, command)}">
+                    <template v-if="scope.row.audit_status === 0 || scope.row.audit_status === 3">
+                        <el-button type="text" @click="detail(scope,3)">审核</el-button>
+                        <el-dropdown trigger="click" style="margin-left: 10px;" @command="(command) => {audit(scope, command)}">
 
-                                    <el-button type="text">
-                                      快捷审核 <i class="el-icon-arrow-down"></i>
-                                    </el-button>
-                                    <el-dropdown-menu slot="dropdown">
-                                        <el-dropdown-item command="1">审核通过</el-dropdown-item>
-                                        <el-dropdown-item command="2">审核不通过</el-dropdown-item>
-                                        <el-dropdown-item  v-if="scope.row.oper_id == 0"  command="3">打回到商户池</el-dropdown-item>
-                                    </el-dropdown-menu>
-                                </el-dropdown>
-                                <!--<el-button type="text" @click="audit(scope, 1)">审核通过</el-button>-->
-                                <!--<el-button type="text" @click="audit(scope, 2)">审核不通过</el-button>-->
-                                <!--<el-button v-if="scope.row.oper_id == 0" type="text" @click="audit(scope, 3)">打回到商户池</el-button>-->
-                            </template>
-                        </template>
-                    </el-table-column>
-                </el-table>
-                <el-pagination
-                        class="fr m-t-20"
-                        layout="total, prev, pager, next"
-                        :current-page.sync="query.page"
-                        @current-change="getList"
-                        :page-size="15"
-                        :total="total"/>
+                            <el-button type="text">
+                              快捷审核 <i class="el-icon-arrow-down"></i>
+                            </el-button>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item command="1">审核通过</el-dropdown-item>
+                                <el-dropdown-item command="2">审核不通过</el-dropdown-item>
+                                <el-dropdown-item  v-if="scope.row.oper_id == 0"  command="3">打回到商户池</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                        <!--<el-button type="text" @click="audit(scope, 1)">审核通过</el-button>-->
+                        <!--<el-button type="text" @click="audit(scope, 2)">审核不通过</el-button>-->
+                        <!--<el-button v-if="scope.row.oper_id == 0" type="text" @click="audit(scope, 3)">打回到商户池</el-button>-->
+                    </template>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+                class="fr m-t-20"
+                layout="total, prev, pager, next"
+                :current-page.sync="query.page"
+                @current-change="getList"
+                :page-size="15"
+                :total="total"/>
 
-            <!--</el-tab-pane>-->
-            <!--&lt;!&ndash;<el-tab-pane label="审核记录" name="audit">&ndash;&gt;-->
-                <!--&lt;!&ndash;<audit-list ref="auditList"/>&ndash;&gt;-->
-            <!--&lt;!&ndash;</el-tab-pane>&ndash;&gt;-->
-        <!--</el-tabs>-->
         <el-dialog :visible.sync="showDetail" width="70%" title="商户详情">
             <merchant-detail :data="currentMerchant" @change="() => {getList(); showDetail = false;}"/>
         </el-dialog>
@@ -164,7 +157,6 @@
 <script>
     import api from '../../../assets/js/api'
     import MerchantDetail from './merchant-detail'
-    import AuditList from './audit-record-list'
     import UnauditMessage from './unaudit-message'
 
     export default {
@@ -270,7 +262,6 @@
         },
         components: {
             MerchantDetail,
-            AuditList,
             UnauditMessage
         }
     }
