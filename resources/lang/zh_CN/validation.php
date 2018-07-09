dee10092b398d8f46c09fcf98a75225d0bd3700a
liwen <wen.li@shoptps.com>
2018-07-09 14:39:09 +0800

    【小程序】买单时，输... https://www.tapd.cn/22216871/s/1002552 --bug=1000366 --user=李文

--- resources/lang/zh_CN/validation.php
+++ resources/lang/zh_CN/validation.php
@@ -93,6 +93,13 @@ return [
         ],
         'price' => [
             'required' => '销售价格不能为空',
+            'min' => '价格需大于 :min',
+            'numeric' => '价格必须为数字',
+        ],
+        'merchant_id' => [
+            'required' => '商户ID不能为空',
+            'integer' => '商户ID必须为整数',
+            'min' => '商户ID不能小于 :min',
         ],
 
         'settlement_rate' => [
