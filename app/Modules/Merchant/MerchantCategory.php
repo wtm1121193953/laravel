efa365a578b3be056a53cddd6d9c498231d37269
Evan <574583188@qq.com>
2018-07-18 12:28:53 +0800

    修复获取分类路径时可能出现死循环的bug

--- app/Modules/Merchant/MerchantCategory.php
+++ app/Modules/Merchant/MerchantCategory.php
@@ -27,7 +27,7 @@ class MerchantCategory extends BaseModel
     public static function getCategoryPath($id)
     {
         $category = MerchantCategory::find($id);
-        if($category->pid > 0){
+        if($category->pid > 0 && $category->pid != $id){
             $parentPath = self::getCategoryPath($category->pid);
         }else {
             $parentPath = [];
