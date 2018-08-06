9f8e0a8429a64159f0ad429e7d4189cdf309fbcc
arno.tang <arno.tang@shoptps.com>
2018-08-06 09:35:16 +0800

    TPS账号绑定需求

--- app/Support/TpsApi.php
+++ app/Support/TpsApi.php
@@ -8,6 +8,7 @@
 
 namespace App\Support;
 
+use App\Support\Common;
 
 class TpsApi
 {
@@ -27,6 +28,27 @@ class TpsApi
     public static function createTpsAccount($account, $password, $parentAccount='', $type=1)
     {
         // todo
+        $data = array(
+            'account' => $account,
+            'password' => $password,
+            'from' => $type,
+            'p_account' =>$parentAccount
+        );
+        $key = config('tpsapi.key');
+        
+        $enc_data = TpsApi::api_encrypt(json_encode($data),$key);
+        $enc_token = TpsApi::api_encrypt($key,$key);
+        
+        $post_data = array(
+            'token' => $enc_token,
+            'data' => $enc_data,
+        );
+        
+        $url = config('tpsapi.register_url');
+        
+        $result = Common::curl_post($url,$post_data);
+        return var_dump($result);
+
     }
 
     /**
@@ -38,6 +60,41 @@ class TpsApi
     {
         // todo
     }
-
+    
+    /**
+     * 加密函数
+     * @param $string
+     * @param $key
+     * return string
+     */
+    public static function api_encrypt($string,$key='') {
+        
+        $encryptKey = md5($key);
+        $keyLen = strlen($encryptKey);
+        $data = substr(md5($string.$encryptKey), 0, 8).$string;
+        $dataLen = strlen($data);
+        $rndkey = array();
+        $box = array();
+        $cipherText = "";
+        for ($i = 0; $i < 256; $i++) {
+            $rndkey[$i] = ord($encryptKey[$i % $keyLen]);
+            $box[$i] = $i;
+        }
+        for ($i = 0, $j = 0; $i < 256; $i++) {
+            $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+            $tmp = $box[$i];
+            $box[$i] = $box[$j];
+            $box[$j] = $tmp;
+        }
+        for ($i = 0, $j = 0, $k = 0; $i < $dataLen; $i++) {
+            $k = ($k + 1) % 256;
+            $j = ($j + $box[$k]) % 256;
+            $tmp = $box[$k];
+            $box[$k] = $box[$j];
+            $box[$j] = $tmp;
+            $cipherText .= chr(ord($data[$i]) ^ ($box[($box[$k] + $box[$j]) % 256]));
+        }
+        return str_replace('=', '', base64_encode($cipherText));
+    }
 
 }
