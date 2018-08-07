2f6ba9d0c7091c9dd294eca9dd7a5062b74a76db
arno <arno.tang@shoptps.com>
2018-08-07 17:23:34 +0800

    TPS绑定账号需求：配置发送邮件地址

--- config/tpsapi.php
+++ config/tpsapi.php
@@ -1,6 +1,7 @@
 <?php 
 return [
-    'register_url' => env('TPS_APIURL') . '/api/daqian/register',
-    'check_user_url' => env('TPS_APIURL') . '/api/user/check_user',
-	'key' => env('TPS_APIKEY'),
+    'register_url'   => env('TPS_APIURL').'/api/daqian/register',
+    'check_url'      => env('TPS_APIURL').'/api/daqian/check_user',
+	'key'            => env('TPS_APIKEY'),
+	'mail_url'       => env('TPS_MAILURL').'/order/sendMail',
 ];
