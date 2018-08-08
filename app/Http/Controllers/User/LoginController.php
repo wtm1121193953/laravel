d9f27396eef20aaf49b3833735f1a0de0c683e2b
liwen <wen.li@shoptps.com>
2018-08-08 12:28:06 +0800

    user 添加微信头像和昵称

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -60,14 +60,20 @@ class LoginController extends Controller
             $verifyCodeRecord->save();
         }
 
+        $wxUserInfo = request('userInfo');
         // 验证通过, 查询当前用户是否存在, 不存在则创建用户
-        if(! $user = User::where('mobile', $mobile)->first()){
+        $user = User::where('mobile', $mobile)->first();
+        if(!$user){
             $user = new User();
             $user->mobile = $mobile;
-            $user->save();
-            // 重新查一次用户信息, 补充用户信息中的全部字段
-            $user = User::find($user->id);
         }
+        if ($wxUserInfo) {
+            $user->wx_nick_name = $wxUserInfo->nickName;
+            $user->wx_avatar_url = $wxUserInfo->avatarUrl;
+        }
+        $user->save();
+        // 重新查一次用户信息, 补充用户信息中的全部字段
+        $user = User::find($user->id);
 
         // 如果存在邀请渠道ID, 查询用户是否已被邀请过
         $inviteChannelId = request('inviteChannelId');
