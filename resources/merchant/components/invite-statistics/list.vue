b7ec887dab32c46936ebb1093ca57e854866f6e5
liwen <wen.li@shoptps.com>
2018-08-08 17:39:36 +0800

    【商户中心】我的会员... https://www.tapd.cn/22216871/s/1006632 --bug=1000779 --user=李文 【商户中心】我的会员页面，微信昵称没有数据

--- resources/merchant/components/invite-statistics/list.vue
+++ resources/merchant/components/invite-statistics/list.vue
@@ -16,7 +16,7 @@
         <el-table :data="list" stripe>
             <el-table-column prop="created_at" label="注册日期"></el-table-column>
             <el-table-column prop="mobile" label="手机号码"></el-table-column>
-            <el-table-column prop="nick_name" label="微信昵称"></el-table-column>
+            <el-table-column prop="wx_nick_name" label="微信昵称"></el-table-column>
             <el-table-column prop="order_number" label="下单次数"></el-table-column>
         </el-table>
         <el-pagination
