356820d8c0ff0d0c8fa83d07dffe7b819985fc74
Evan <574583188@qq.com>
2018-08-09 15:44:27 +0800

    TpsBindService添加方法定义

--- app/Modules/Tps/TpsBindService.php
+++ app/Modules/Tps/TpsBindService.php
@@ -16,43 +16,55 @@ class TpsBindService extends BaseService
 {
 
     /**
-     * 根据账号查询TPS绑定信息
-     * @param $tps_bind_account
+     * 根据用户ID及类型获取用户绑定的tps账号
+     * @param $originId
+     * @param $originType
      * @return TpsBind
      */
-	public static function getBindByaccount($tps_bind_account)
+    public static function getTpsBindInfoByOriginInfo($originId, $originType)
     {
-    	return TpsBind::where('tps_bind_account', $tps_bind_account)->first();
+        return TpsBind::where('origin_id', $originId)
+            ->where('origin_type', $originType)->first();
     }
 
     /**
-     * 添加账号
-     * @param $operId int
-     * @param $account string
-     * @param $password string
-     * @return OperAccount
+     * 根据tps账号获取该tps账号绑定的信息
+     * @param $tpsAccount
+     * @return TpsBind
      */
-    public static function createAccount($operId, $account, $password)
+    public static function getTpsBindInfoByTpsAccount($tpsAccount)
     {
+        // todo 根据tps账号获取该tps账号绑定的信息
+    }
 
-        $obj = self::getByOperId($operId);
-        if(!empty($obj)){
-            throw new BaseResponseException('该运营中心账户已存在, 不能重复创建', ResultCode::ACCOUNT_EXISTS);
-        }
-        // 查询账号是否重复
-        if(!empty(OperAccount::where('account', request('account'))->first())){
-            throw new BaseResponseException('帐号重复, 请更换帐号');
-        }
-        $obj = new OperAccount();
+    /**
+     * 运营中心绑定TPS账号
+     * @param $operId
+     * @param $tpsAccount
+     */
+    public static function bindTpsAccountForOper($operId, $tpsAccount)
+    {
+        // todo 运营中心绑定账号逻辑
+    }
 
-        $obj->account = $account;
-        $obj->oper_id = $operId;
-        $salt = str_random();
-        $obj->salt = $salt;
-        $obj->password = OperAccount::genPassword($password, $salt);
-        $obj->save();
+    /**
+     * 商户绑定TPS账号
+     * @param $merchantId
+     * @param $tpsAccount
+     */
+    public static function bindTpsAccountForMerchant($merchantId, $tpsAccount)
+    {
+        // TODO 商户绑定TPS账号
+    }
 
-        return $obj;
+    /**
+     * 用户绑定TPS账号逻辑
+     * @param $userId
+     * @param $tpsAccount
+     */
+    public static function bindTpsAccountForUser($userId, $tpsAccount)
+    {
+        // todo 用户绑定TPS账号逻辑
     }
 
 }
