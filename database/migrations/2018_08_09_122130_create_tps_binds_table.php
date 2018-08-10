720a3ef31a22774977624806a9892fd4c598da91
Evan <574583188@qq.com>
2018-08-10 10:33:41 +0800

    修改tps_binds表tps_account字段类型

--- database/migrations/2018_08_09_122130_create_tps_binds_table.php
+++ database/migrations/2018_08_09_122130_create_tps_binds_table.php
@@ -17,7 +17,7 @@ class CreateTpsBindsTable extends Migration
             $table->increments('id');
             $table->tinyInteger('origin_type')->index()->default(0)->comment('绑定的用户类型，1：用户，2：商户，3：运营中心。');
             $table->integer('origin_id')->index()->default(0)->comment('用户ID, 用户的是users表ID, 商户是merchants表ID, 运营中心是oper表ID');
-            $table->tinyInteger('tps_account')->default(0)->comment('绑定的TPS账号');
+            $table->string('tps_account')->default('')->comment('绑定的TPS账号');
             $table->timestamps();
 
             $table->comment = 'TPS账号绑定表';
