a86b7f7d3e46ca2ad7bcd0d5d71fe4d302e9bbdc
liwen <wen.li@shoptps.com>
2018-08-10 15:45:41 +0800

    【运营中心】渠道列表... https://www.tapd.cn/22216871/s/1007183 --bug=1000830 --user=李文 oper 渠道列表里，手机号码缺少删除功能

--- resources/oper/components/invite-record/list.vue
+++ resources/oper/components/invite-record/list.vue
@@ -23,7 +23,7 @@
                 ></el-date-picker>
             </el-form-item>
             <el-form-item prop="mobile" label="手机号">
-                <el-input v-model="query.mobile" placeholder="请输入手机号" @keyup.enter.native="search"></el-input>
+                <el-input v-model="query.mobile" placeholder="请输入手机号" @keyup.enter.native="search" clearable></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="search"><i class="el-icon-search">搜索</i></el-button>
