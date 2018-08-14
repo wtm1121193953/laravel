3e8616212d489f43de5e3fdee6182033eda1ba68
cherry_fu <cherry.fu@shoptps.com>
2018-08-14 18:43:15 +0800

    业务员管理后台-运营中心列表制作

--- resources/bizer/components/oper/list.vue
+++ resources/bizer/components/oper/list.vue
@@ -1,15 +1,26 @@
 <template>
-    <page title="我的商户" v-loading="isLoading">
+    <page title="运营中心列表" v-loading="isLoading">
         <el-form class="fl" inline size="small">
-            <el-form-item label="" prop="name">
-                <el-input v-model="query.name" @keyup.enter.native="search" clearable placeholder="商户名称"/>
+            <el-form-item prop="createdAt" label="添加时间">
+                <el-date-picker
+                        v-model="query.createdAt"
+                        type="daterange"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        value-format="yyyy-MM-dd">
+                </el-date-picker>
             </el-form-item>
-
-            <el-form-item prop="signboardName" label="商户招牌名" >
-                <el-input v-model="query.signboardName" size="small" placeholder="商家招牌名" clearable @keyup.enter.native="search"/>
+            <el-form-item label="" prop="name" label="运营中心名称">
+                <el-input v-model="query.name" placeholder="请输入运营中心名称" clearable @keyup.enter.native="search"/>
             </el-form-item>
-
-            <el-form-item prop="merchant_category" label="所属行业">
+            <el-form-item prop="principal" label="负责人">
+                <el-input v-model="query.principal" placeholder="请输入负责人" clearable @keyup.enter.native="search"/>
+            </el-form-item>
+            <el-form-item prop="contactNumber" label="联系电话">
+                <el-input v-model="query.contactNumber" placeholder="请输入联系电话" clearable @keyup.enter.native="search"/>
+            </el-form-item>
+            <el-form-item prop="city" label="所在城市">
                 <el-cascader
                         change-on-select
                         clearable
@@ -20,106 +31,49 @@
                             label: 'name',
                             children: 'sub',
                         }"
-                        v-model="query.merchant_category">
+                        v-model="query.city">
                 </el-cascader>
             </el-form-item>
-
-            <el-form-item label="状态" prop="status">
+            <el-form-item prop="status" label="状态">
                 <el-select v-model="query.status" class="w-100">
                     <el-option label="全部" value=""/>
                     <el-option label="正常" value="1"/>
-                    <el-option label="已冻结" value="2"/>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="审核状态" prop="audit_status">
-                <el-select v-model="query.audit_status" placeholder="请选择">
-                    <el-option label="全部" value=""/>
-                    <el-option label="待审核" value="-1"/>
-                    <el-option label="审核通过" value="1"/>
-                    <el-option label="审核不通过" value="2"/>
-                    <el-option label="重新提交审核" value="3"/>
+                    <el-option label="申请中" value="2"/>
                 </el-select>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="search"><i class="el-icon-search">搜索</i></el-button>
+                <el-button type="primary" icon="el-icon-search" @click="search">搜索</el-button>
             </el-form-item>
         </el-form>
-        <!--<el-dropdown class="fr" @command="addBtnClick" trigger="click">
-            <el-button type="primary">
-                添加商户<i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>
-            </el-button>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item command="from-pool">从商户池添加</el-dropdown-item>
-                <el-dropdown-item command="add">添加新商户</el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>-->
-        <el-button class="fr" type="primary" @click="add">录入并激活商户</el-button>
+
+        <el-button class="fr" type="primary" icon="el-icon-plus" @click="add">添加运营中心</el-button>
+
         <el-table :data="list" stripe>
             <el-table-column prop="created_at" label="添加时间"/>
-            <el-table-column prop="id" label="ID"/>
-            <el-table-column prop="name" label="商户名称"/>
-            <el-table-column prop="signboard_name" label="商户招牌名"/>
-            <el-table-column prop="categoryPath" label="行业">
+            <el-table-column prop="id" label="运营中心名称"/>
+            <el-table-column prop="name" label="负责人"/>
+            <el-table-column prop="signboard_name" label="联系电话"/>
+            <el-table-column prop="city" label="所在城市">
                 <template slot-scope="scope">
-                    <span v-for="item in scope.row.categoryPath" :key="item.id">
-                        {{ item.name }}
-                    </span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="city" label="城市">
-                <template slot-scope="scope">
-                    <!--<span> {{ scope.row.province }} </span>-->
+                    <!-- <span> {{ scope.row.province }} </span> -->
                     <span> {{ scope.row.city }} </span>
                     <span> {{ scope.row.area }} </span>
                 </template>
             </el-table-column>
-            <el-table-column prop="operBizMemberName" label="业务员"/>
-            <el-table-column prop="status" label="商户状态">
-                <template slot-scope="scope" v-if="scope.row.audit_status == 1 || scope.row.audit_status == 3">
-                    <span v-if="scope.row.status === 1" class="c-green">正常</span>
-                    <span v-else-if="scope.row.status === 2" class="c-danger">已冻结</span>
-                    <span v-else>未知 ({{scope.row.status}})</span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="audit_status" label="审核状态">
+            <el-table-column prop="operBizMemberName" label="我的分成"/>
+            <el-table-column prop="status" label="合作状态">
                 <template slot-scope="scope">
-                    <span v-if="parseInt(scope.row.audit_status) === 0" class="c-warning">待审核</span>
-                    <el-popover
-                            v-else-if="scope.row.audit_status === 1"
-                            placement="bottom-start"
-                            width="200px"  trigger="hover"
-                            @show="showMessage(scope)"
-                            :disabled="scope.row.audit_suggestion == ''">
-                        <div   slot="reference" class="c-green"><p>审核通过</p><span class="message">{{scope.row.audit_suggestion}}</span></div>
-                        <!-- <unaudit-record-reason    :data="auditRecord"  /> -->
-                    </el-popover>
-
-                    <el-popover
-                            v-else-if="parseInt(scope.row.audit_status) === 2"
-                            placement="bottom-start"
-                            width="200px"  trigger="hover"
-                            @show="showMessage(scope)"
-                            :disabled="scope.row.audit_suggestion == ''" >
-                        <div   slot="reference" class="c-danger"><p>审核不通过</p><span class="message">{{scope.row.audit_suggestion}}</span></div>
-                        <!-- <unaudit-record-reason    :data="auditRecord"  /> -->
-                    </el-popover>
-
-
-                    <span v-else-if="parseInt(scope.row.audit_status) === 3" class="c-warning">重新提交审核中</span>
-                    <span v-else>未知 ({{scope.row.audit_status}})</span>
+                    <span v-if="scope.row.status === 1">正常</span>
+                    <span v-else-if="scope.row.status === 2" class="c-danger">申请中</span>
                 </template>
             </el-table-column>
             <el-table-column label="操作" width="250px">
                 <template slot-scope="scope">
-                    <!-- <merchant-item-options
-                            :scope="scope"
-                            :query="query"
-                            @change="itemChanged"
-                            @accountChanged="accountChanged"
-                            @refresh="getList"/> -->
+                    <el-button type="text">查看商户</el-button>
                 </template>
             </el-table-column>
         </el-table>
+
         <el-pagination
                 class="fr m-t-20"
                 layout="total, prev, pager, next"
@@ -127,6 +81,24 @@
                 @current-change="getList"
                 :page-size="15"
                 :total="total"/>
+
+        <el-dialog title="添加运营中心" :visible.sync="dialogFormVisible" width="30%">
+            <el-form :model="form">
+                <el-form-item label="运营中心名称" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择运营中心">
+                        <el-option label="运营中心1" value="1"></el-option>
+                        <el-option label="运营中心2" value="2"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="备注" :label-width="formLabelWidth">
+                    <el-input type="textarea" v-model="form.desc" auto-complete="off"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">发送申请</el-button>
+            </div>
+        </el-dialog>
     </page>
 </template>
 
@@ -145,14 +117,22 @@
                 auditRecord:[],
                 isLoading: false,
                 query: {
+                    createdAt: '',
                     name: '',
+                    principal: '',
+                    contactNumber: '',
                     status: '',
-                    page: 1,
-                    audit_status: '',
-                    signboardName:''
+                    page: 1
                 },
                 list: [],
                 total: 0,
+
+                dialogFormVisible: false,
+                form: {
+                    region: '',
+                    desc: ''
+                },
+                formLabelWidth: '120px'
             }
         },
         computed: {
@@ -174,9 +154,9 @@
                     this.total = data.total;
                 })
             },
-            itemChanged(index, data){
-                this.getList();
-            },
+            // itemChanged(index, data){
+            //     this.getList();
+            // },
             addBtnClick(command){
                 if(command === 'add'){
                     this.add()
@@ -185,12 +165,7 @@
                 }
             },
             add(){
-                router.push({
-                    path: '/merchant/add',
-                    query: {
-                        type: 'merchant-list'
-                    }
-                });
+                this.dialogFormVisible = true;
             },
             showMessage(scope){
                 api.get('/merchant/audit/record/newest', {id: scope.row.id}).then(data => {
@@ -198,12 +173,12 @@
                 })
             },
 
-            accountChanged(scope, account){
-                let row = this.list[scope.$index];
-                row.account = account;
-                this.list.splice(scope.$index, 1, row);
-                this.getList();
-            },
+            // accountChanged(scope, account){
+            //     let row = this.list[scope.$index];
+            //     row.account = account;
+            //     this.list.splice(scope.$index, 1, row);
+            //     this.getList();
+            // },
         },
 
 
