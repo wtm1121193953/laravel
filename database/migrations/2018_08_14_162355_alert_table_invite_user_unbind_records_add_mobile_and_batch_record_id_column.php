a7e54a78ff23b6d6d97c2e0c47bbaf514c22a115
Evan <574583188@qq.com>
2018-08-16 10:06:56 +0800

    修改表名invite_user_change_bind_records为invite_user_batch_changed_records 修改invite_user_unbind_records中的字段change_bind_record_id 为 batch_record_id

--- database/migrations/2018_08_14_162355_alert_table_invite_user_unbind_records_add_mobile_and_batch_record_id_column.php
+++ database/migrations/2018_08_14_162355_alert_table_invite_user_unbind_records_add_mobile_and_batch_record_id_column.php
@@ -16,7 +16,7 @@ class AlertTableInviteUserUnbindRecordsAddMobileAndBatchRecordIdColumn extends M
         Schema::table('invite_user_unbind_records', function (Blueprint $table) {
             $table->string('mobile')->default('')->comment('解绑用户的手机号码')->after('status');
             $table->integer('batch_record_id')->index()->default(0)->comment('关联批量换绑记录表(invite_user_batch_changed_records)的ID')->after('mobile');
-            $table->string('old_invite_user_record', 2000)->default('')->comment('原来的邀请记录')->after('change_bind_record_id');
+            $table->string('old_invite_user_record', 2000)->default('')->comment('原来的邀请记录')->after('batch_record_id');
         });
     }
 
