c09b2366c82e7ffeb0842b7cbc536b46510217a2
Evan <574583188@qq.com>
2018-08-18 17:00:15 +0800

    版本更新命令中添加更新邀请数据统计的操作

--- app/Console/Commands/Updates/V1_4_0.php
+++ app/Console/Commands/Updates/V1_4_0.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands\Updates;
 
+use App\Modules\Invite\InviteStatisticsService;
+use App\Modules\Invite\InviteUserRecord;
+use App\Modules\Invite\InviteUserService;
 use App\Modules\Order\OrderService;
 use App\Modules\User\User;
 use App\Modules\Wechat\MiniprogramScene;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class V1_4_0 extends Command
@@ -41,7 +45,7 @@ class V1_4_0 extends Command
     public function handle()
     {
         // 更新用户下单总次数
-        $this->info('准备更新用户下单总次数');
+        $this->info('更新用户下单总次数 Start');
         $bar = $this->output->createProgressBar(User::count('id'));
         User::chunk(1000, function($list) use ($bar){
             $list->map(function(User $user) use ($bar){
@@ -51,10 +55,27 @@ class V1_4_0 extends Command
             });
         });
         $bar->finish();
+        $this->info("\n更新用户下单总次数 Finished");
 
         // 清除小程序场景表中的小程序码链接, 以便重新生成小程序码
+        $this->info('清除小程序场景表中的小程序码链接 Start');
         MiniprogramScene::where('id', '>', 0)
             ->update(['qrcode_url' => '']);
+        $this->info('清除小程序场景表中的小程序码链接 Finished');
+
+        // 更新邀请记录统计数据
+        $this->info("更新邀请记录统计数据 Start");
+        /** @var Carbon $date */
+        $date = InviteUserRecord::orderBy('created_at')->value('created_at');
+        $totalDays = $date->diffInDays(Carbon::today());
+        $bar = $this->output->createProgressBar($totalDays);
+        while ($date->lt(Carbon::today())){
+            InviteStatisticsService::batchUpdateDailyStatisticsByDate($date);
+            $date->addDay();
+            $bar->advance();
+        }
+        $bar->finish();
+        $this->info("\n更新邀请记录统计数据 Finished");
 
     }
 }
