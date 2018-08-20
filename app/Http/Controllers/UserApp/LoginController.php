a9074302a12ab6c27a3fe4be0694ffe7875c35a3
franmy <franmy.wang@shoptps.com>
2018-08-20 15:05:53 +0800

    userApp模块下LoginController控制器代码抽离

--- app/Http/Controllers/UserApp/LoginController.php
+++ app/Http/Controllers/UserApp/LoginController.php
@@ -11,18 +11,9 @@ namespace App\Http\Controllers\UserApp;
 
 use App\Exceptions\ParamInvalidException;
 use App\Http\Controllers\Controller;
-use App\Modules\Invite\InviteChannelService;
-use App\Modules\Invite\InviteUserService;
-use App\Modules\Merchant\Merchant;
-use App\Modules\Oper\Oper;
-use App\Modules\Sms\SmsVerifyCode;
-use App\Modules\User\User;
-use App\Modules\User\UserMapping;
-use App\Result;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\App;
+use App\Modules\User\UserService;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
+use App\Result;
 
 class LoginController extends Controller
 {
@@ -42,61 +33,12 @@ class LoginController extends Controller
             throw new ParamInvalidException('手机号码不合法');
         }
         $verifyCode = request('verify_code');
-        DB::beginTransaction();
-        // 非正式环境时, 验证码为6666为通过验证
-        if(App::environment('production') || $verifyCode != '6666'){
-            $verifyCodeRecord = SmsVerifyCode::where('mobile', $mobile)
-                ->where('verify_code', $verifyCode)
-                ->where('status', 1)
-                ->where('expire_time', '>', Carbon::now())
-                ->first();
-            if(empty($verifyCodeRecord)){
-                throw new ParamInvalidException('验证码错误');
-            }
-            $verifyCodeRecord->status = 2;
-            $verifyCodeRecord->save();
-        }
 
-        // 验证通过, 查询当前用户是否存在, 不存在则创建用户
-        if(! $user = User::where('mobile', $mobile)->first()){
-            $user = new User();
-            $user->mobile = $mobile;
-            $user->save();
-            // 重新查一次用户信息, 补充用户信息中的全部字段
-            $user = User::find($user->id);
-        }
+        $data = UserService::userAppLogin($mobile,$verifyCode);
 
-        // 如果存在邀请渠道ID, 查询用户是否已被邀请过
-        $inviteChannelId = request('inviteChannelId');
-        if($inviteChannelId){
-            $inviteChannel = InviteChannelService::getById($inviteChannelId);
-            if(empty($inviteChannel)){
-                throw new ParamInvalidException('邀请渠道不存在');
-            }
-            InviteUserService::bindInviter($user->id, $inviteChannel);
-        }
-        // 生成token并返回
-        $token = str_random(64);
-        Cache::put('token_to_user_' . $token, $user, 60 * 24 * 30);
-
-        DB::commit();
-
-        $userMapping = UserMapping::where('user_id', $user->id)->first();
-        if (!empty($userMapping)){
-            if ($userMapping->origin_type == 1){
-                $merchant = Merchant::findOrFail($userMapping->origin_id);
-                $user->mapping_merchant_name = $merchant->name;
-                $user->merchant_level = $merchant->level;
-                $user->merchant_level_text = Merchant::getLevelText($merchant->level);
-            }else{
-                $oper = Oper::findOrFail($userMapping->origin_id);
-                $user->mapping_oper_name = $oper->name;
-            }
-        }
-        $user->level_text = User::getLevelText($user->level);
         return Result::success([
-            'userInfo' => $user,
-            'token' => $token
+            'userInfo' => $data['user'],
+            'token' => $data['token']
         ]);
     }
 
--- app/Modules/User/UserService.php
+++ app/Modules/User/UserService.php
@@ -9,12 +9,88 @@
 namespace App\Modules\User;
 
 use App\BaseService;
+use App\Exceptions\ParamInvalidException;
 use App\Modules\Invite\InviteUserService;
 use Illuminate\Database\Eloquent\Builder;
+use App\Modules\Invite\InviteChannelService;
+use App\Modules\Merchant\Merchant;
+use App\Modules\Oper\Oper;
+use App\Modules\Sms\SmsVerifyCode;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class UserService extends BaseService
 {
 
+
+    /**
+     * @param $mobile
+     * @param $verifyCode
+     * @return array
+     * @throws \Exception
+     */
+    public static function userAppLogin($mobile, $verifyCode)
+    {
+        DB::beginTransaction();
+        // 非正式环境时, 验证码为6666为通过验证
+        if(App::environment('production') || $verifyCode != '6666'){
+            $verifyCodeRecord = SmsVerifyCode::where('mobile', $mobile)
+                ->where('verify_code', $verifyCode)
+                ->where('status', 1)
+                ->where('expire_time', '>', Carbon::now())
+                ->first();
+            if(empty($verifyCodeRecord)){
+                throw new ParamInvalidException('验证码错误');
+            }
+            $verifyCodeRecord->status = 2;
+            $verifyCodeRecord->save();
+        }
+
+        // 验证通过, 查询当前用户是否存在, 不存在则创建用户
+        if(! $user = User::where('mobile', $mobile)->first()){
+            $user = new User();
+            $user->mobile = $mobile;
+            $user->save();
+            // 重新查一次用户信息, 补充用户信息中的全部字段
+            $user = User::find($user->id);
+        }
+
+        // 如果存在邀请渠道ID, 查询用户是否已被邀请过
+        $inviteChannelId = request('inviteChannelId');
+        if($inviteChannelId){
+            $inviteChannel = InviteChannelService::getById($inviteChannelId);
+            if(empty($inviteChannel)){
+                throw new ParamInvalidException('邀请渠道不存在');
+            }
+            InviteUserService::bindInviter($user->id, $inviteChannel);
+        }
+        // 生成token并返回
+        $token = str_random(64);
+        Cache::put('token_to_user_' . $token, $user, 60 * 24 * 30);
+
+        DB::commit();
+
+        $userMapping = UserMapping::where('user_id', $user->id)->first();
+        if (!empty($userMapping)){
+            if ($userMapping->origin_type == 1){
+                $merchant = Merchant::findOrFail($userMapping->origin_id);
+                $user->mapping_merchant_name = $merchant->name;
+                $user->merchant_level = $merchant->level;
+                $user->merchant_level_text = Merchant::getLevelText($merchant->level);
+            }else{
+                $oper = Oper::findOrFail($userMapping->origin_id);
+                $user->mapping_oper_name = $oper->name;
+            }
+        }
+        $user->level_text = User::getLevelText($user->level);
+
+        return [
+            'user' => $user,
+            'token' => $token
+        ];
+    }
     /**
      * 获取会员列表
      * @param $params
