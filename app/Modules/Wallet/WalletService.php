47945d207bb5aa75c414a7e9b503851411081ef8
bruse <1066771229@qq.com>
2018-08-21 12:17:19 +0800

    修改表结构

---
+++ app/Modules/Wallet/WalletService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 57458
+ * Date: 2018/8/21
+ * Time: 12:00
+ */
+
+namespace App\Modules\Wallet;
+
+
+use App\BaseService;
+
+class WalletService extends BaseService
+{
+
+    /**
+     * 增加余额
+     */
+    public static function addBalance()
+    {
+
+    }
+}
--- database/migrations/2018_08_20_222235_create_wallet_consume_quota_records_table.php
+++ database/migrations/2018_08_20_222235_create_wallet_consume_quota_records_table.php
@@ -21,6 +21,7 @@ class CreateWalletConsumeQuotaRecordsTable extends Migration
             $table->tinyInteger('type')->index()->default(1)->comment('来源类型 1-消费自返 2-直接下级消费返');
             $table->integer('order_id')->index()->default(0)->comment('分润的订单ID');
             $table->string('order_no')->index()->default('')->comment('分润的订单号');
+            $table->decimal('order_profit_amount', 8, 2)->default(0)->comment('订单利润');
             $table->decimal('consume_quota', 8, 2)->default(0)->comment('消费额');
             $table->string('consume_user_mobile', 11)->default('')->comment('消费用户手机号');
             $table->tinyInteger('status')->index()->default(1)->comment('状态 1-冻结中 2-已解冻待置换 3-已置换 4-已退款');
