12c6a31b4f5ddaa7328b2e64fe4241309522cfa6
franmy <franmy.wang@shoptps.com>
2018-08-22 21:48:27 +0800

    表字段修改01

--- database/migrations/2018_08_22_211720_alert_settlements_table_add_pay_target_type_and_remark.php
+++ database/migrations/2018_08_22_211720_alert_settlements_table_add_pay_target_type_and_remark.php
@@ -15,7 +15,7 @@ class AlertSettlementsTableAddPayTargetTypeAndRemark extends Migration
     {
         Schema::table('settlements', function (Blueprint $table) {
             $table->tinyInteger('pay_target_type')->index()->default(1)->comment('1-支付给运营中心  2-支付到平台');
-            $table->text('remark')->default('')->comment('备注');
+            $table->string('remark')->default('')->comment('备注');
         });
     }
 
