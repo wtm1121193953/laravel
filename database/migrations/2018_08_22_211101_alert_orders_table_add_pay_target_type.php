52097497173650fc95e7d6dc1524fd8d9e455e5b
franmy <franmy.wang@shoptps.com>
2018-08-22 21:39:57 +0800

    表字段修改

---
+++ database/migrations/2018_08_22_211101_alert_orders_table_add_pay_target_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlertOrdersTableAddPayTargetType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->tinyInteger('pay_target_type')->index()->default(1)->comment('1-支付给运营中心  2-支付到平台');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn([
+                'pay_target_type',
+            ]);
+        });
+    }
+}
