3bdb2d0c5277b8bac1d3b81898435f2358c469d0
Evan <574583188@qq.com>
2018-08-22 21:56:38 +0800

    添加融宝支付类

---
+++ app/Support/ReapalPay.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Support;
+
+/**
+ * 融宝支付
+ * Class ReapalPay
+ * @package App\Support
+ */
+class ReapalPay
+{
+    
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * 代付提交
+     */
+    public static function agentpay()
+    {
+
+    }
+
+    /**
+     * 代付批次查询
+     */
+    public static function queryAgentpayBatch()
+    {
+
+    }
+
+    /**
+     * 代付单笔查询
+     */
+    public static function queryAgentpaySingle()
+    {
+
+    }
+
+    /**
+     * 代付账户查询余额
+     */
+    public static function queryBalance()
+    {
+
+    }
+}
