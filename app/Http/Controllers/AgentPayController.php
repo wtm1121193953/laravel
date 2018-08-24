5e8cc26d45969f9369299bb8e0a8d6939e6d5d88
Evan <574583188@qq.com>
2018-08-24 20:18:54 +0800

    添加代付回调控制器

---
+++ app/Http/Controllers/AgentPayController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 57458
+ * Date: 2018/8/24
+ * Time: 20:17
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * 代付相关控制器
+ * Class AgentPayController
+ * @package App\Http\Controllers
+ */
+class AgentPayController extends Controller
+{
+
+    /**
+     * 荣宝代付单回调地址
+     */
+    public function reapalNotify()
+    {
+        // 处理融宝代付单异步通知
+    }
+}
