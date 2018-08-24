0e0172808d68e0d333868ce6fc15c3e52feeef7b
cherry_fu <cherry.fu@shoptps.com>
2018-08-24 15:31:53 +0800

    修改业务员申请列表

--- resources/oper/components/oper-biz-member/record.vue
+++ resources/oper/components/oper-biz-member/record.vue
@@ -1,62 +1,41 @@
 <template>
     <page title="业务员申请" v-loading="isLoading">
         <el-tabs type="card" v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="新申请10" name="first">
-                <el-table :data="list" stripe>
-                    <el-table-column prop="created_at" label="申请签约时间"/>
-                    <el-table-column prop="name" label="姓名">
-                        <template slot-scope="scope">
-                            <span> {{ scope.row.operInfo.name }} </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="tel" label="手机号">
-                        <template slot-scope="scope">
-                            <span> {{ scope.row.operInfo.tel }} </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column fixed="right" label="操作">
-                        <template slot-scope="scope">
-                            <el-button type="text" @click="signing">签约</el-button>
-                            <el-button type="text" @click="refusal">拒绝</el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
-
-                <el-pagination
-                        class="fr m-t-20"
-                        layout="total, prev, pager, next"
-                        :current-page.sync="query.page"
-                        @current-change="getList"
-                        :page-size="15"
-                        :total="total"/>
-            </el-tab-pane>
-
-            <el-tab-pane label="已拒绝" name="second">
-                <el-table :data="list" stripe>
-                    <el-table-column prop="created_at" label="申请签约时间"/>
-                    <el-table-column prop="name" label="姓名">
-                        <template slot-scope="scope">
-                            <span> {{ scope.row.operInfo.name }} </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="tel" label="手机号">
-                        <template slot-scope="scope">
-                            <span> {{ scope.row.operInfo.tel }} </span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="updated_at" label="拒绝签约时间"/>
-                    <el-table-column prop="remark" label="原因"/>
-                </el-table>
-
-                <el-pagination
-                        class="fr m-t-20"
-                        layout="total, prev, pager, next"
-                        :current-page.sync="query.page"
-                        @current-change="getList"
-                        :page-size="15"
-                        :total="total"/>
-            </el-tab-pane>
+            <el-tab-pane label="新申请10" name="first"></el-tab-pane>
+            <el-tab-pane label="已拒绝" name="second"></el-tab-pane>
         </el-tabs>
+
+        <el-table :data="list" stripe>
+            <el-table-column prop="created_at" label="申请签约时间"/>
+            <el-table-column prop="name" label="姓名">
+                <template slot-scope="scope">
+                    <span> {{ scope.row.operInfo.name }} </span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="tel" label="手机号">
+                <template slot-scope="scope">
+                    <span> {{ scope.row.operInfo.tel }} </span>
+                </template>
+            </el-table-column>
+
+            <el-table-column prop="updated_at" label="拒绝签约时间" v-if="secondTable"/>
+            <el-table-column prop="remark" label="原因" v-if="secondTable"/>
+
+            <el-table-column fixed="right" label="操作" v-if="firstTable">
+                <template slot-scope="scope">
+                    <el-button type="text" @click="signing">签约</el-button>
+                    <el-button type="text" @click="refusal">拒绝</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <el-pagination
+                class="fr m-t-20"
+                layout="total, prev, pager, next"
+                :current-page.sync="query.page"
+                @current-change="getList"
+                :page-size="15"
+                :total="total"/>
 
         <el-dialog title="签约业务员" :visible.sync="dialogSigningFormVisible" width="30%">
             <el-form :model="formSigning" :rules="rules" label-width="70px">
@@ -101,6 +80,8 @@
             return {
                 isLoading: false,
                 activeName: 'first',
+                firstTable: true,
+                secondTable: false,
                 query: {
                     page: 1
                 },
@@ -142,6 +123,14 @@
             },
             handleClick(tab, event) {
                 // console.log(tab, event);
+                let _self = this;
+                if ( _self.activeName == "first" ) {
+                    _self.secondTable = false;
+                    // _self.firstTable = true;
+                } else if ( _self.activeName == "second" ) {
+                    _self.secondTable = true;
+                    // _self.firstTable = false;
+                }
             },
             signing() {
                 this.dialogSigningFormVisible = true;
