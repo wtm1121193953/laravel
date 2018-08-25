62d1c46535e3b33b8979d33c3bdad5c22266284c
bruse <1066771229@qq.com>
2018-08-25 18:40:09 +0800

    修改索引名称

--- database/migrations/2018_08_20_222811_create_wallet_consume_quota_unfreeze_records_table.php
+++ database/migrations/2018_08_20_222811_create_wallet_consume_quota_unfreeze_records_table.php
@@ -16,11 +16,13 @@ class CreateWalletConsumeQuotaUnfreezeRecordsTable extends Migration
         Schema::create('wallet_consume_quota_unfreeze_records', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('wallet_id')->index()->default(0)->comment('钱包ID');
-            $table->integer('consume_quota_record_id')->index()->default(0)->comment('消费额记录表id');
+            $table->integer('consume_quota_record_id')->default(0)->comment('消费额记录表id');
             $table->integer('origin_id')->index()->default(0)->comment('用户ID');
             $table->tinyInteger('origin_type')->index()->default(1)->comment('用户类型 1-用户 2-商户 3-运营中心');
             $table->decimal('unfreeze_consume_quota', 8, 2)->default(0)->comment('解冻消费额数量');
             $table->timestamps();
+
+            $table->index('consume_quota_record_id', 'consume_quota_record_id_index');
         });
     }
 
