06265173153285f517a1575b526b3d301559b804
liwen <wen.li@shoptps.com>
2018-09-01 14:26:00 +0800

    平台支付小程序 配置

--- config/platform.php
+++ config/platform.php
@@ -1,11 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\App;
-
 return [
     'miniprogram' => [
-        'app_id' => App::environment('production') ? 'wxdc74ddcdb8389b98' : 'wx8d0f5e945df699c2',
-        'app_secret' => App::environment('production') ? '5e2648fc79ccd467f6abadb3347d813e' : '8f5fed3b1edfa44d409416db60f22bfb',
+        'app_id' => env('APP_ENV') == 'production' ? 'wxdc74ddcdb8389b98' : 'wx8d0f5e945df699c2',
+        'app_secret' => env('APP_ENV') == 'production' ? '5e2648fc79ccd467f6abadb3347d813e' : '8f5fed3b1edfa44d409416db60f22bfb',
     ],
     'wechat_pay' => [
         'mch_id' => '',
