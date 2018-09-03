ca006d68f91a2e7dd3eaf5810c657bd16b236188
zwg <wenguang.zhang@shoptps.com>
2018-09-03 15:16:09 +0800

    添加新接口(交易密码)

--- app/Http/Controllers/UserApp/WalletController.php
+++ app/Http/Controllers/UserApp/WalletController.php
@@ -16,6 +16,8 @@ use App\Modules\Wallet\WalletService;
 use App\Modules\Wallet\ConsumeQuotaService;
 use App\Modules\Wallet\WalletConsumeQuotaRecord;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
 //用户账单
 use App\Modules\Wallet\WalletBill;
 
@@ -48,7 +50,7 @@ class WalletController extends Controller
             'originType' => WalletBill::ORIGIN_TYPE_USER,
             'startDate' => $startDate,
             'endDate' => $endDate,
-            'typeArr' => [$type],
+            'typeArr' => $type,
         ], 20);
 
         return Result::success([
@@ -128,6 +130,97 @@ class WalletController extends Controller
         return Result::success($consumeQuota);
     }
 
+    /**
+     * 校验旧密码
+     * Author:  zwg
+     * Date:    180903
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function confirmPassword()
+    {
+        $this->validate(request(), [
+            'password'  =>  'required|numeric'
+        ]);
+        $user = request()->get('current_user');
+        WalletService::checkPayPassword( request()->input('password'), $user->id);
+        // 记录确认密码时间
+        $token = str_random();
+        Cache::put('user_pay_password_modify_temp_token_' . $user->id, $token, 3);
+        return Result::success([
+            'temp_token' => $token
+        ]);
+    }
+
+    /**
+     * 重置密码
+     * Author:  zwg
+     * Date:    180903
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function changePassword()
+    {
+
+        $currentUser =  request()->get('current_user');
+
+        $wallet = WalletService::getWalletInfoByOriginInfo($currentUser->id, Wallet::ORIGIN_TYPE_USER);
+
+        if( !empty($wallet['withdraw_password']) )
+        {
+            // 如果已设置过密码，则走以下逻辑
+            $this->validate(request(), [
+                'temp_token' => 'required'
+            ]);
+            $inputToken = request()->get('temp_token');
+            $user = request()->get('current_user');
+            $tempToken = Cache::get('user_pay_password_modify_temp_token_' . $user->id);
+            if(empty($tempToken)){
+                throw new NoPermissionException('您的验证信息已超时, 请返回重新验证');
+            }
+            if($tempToken != $inputToken){
+                throw new NoPermissionException('验证信息无效');
+            }
+            // 删除有效时间，避免重复提交
+            Cache::forget('user_pay_password_modify_temp_token_' . $user->id);
+        }
+
+        $this->validate(request(), [
+            'password'  =>  'required|numeric'
+        ]);
+        // 重置密码入库
+        $res = WalletService::updateWalletWithdrawPassword( $wallet, request()->input('password'));
+        if( $res )
+        {
+            return Result::success('重置密码成功');
+        }
+        throw new BaseResponseException('重置密码失败', ResultCode::DB_UPDATE_FAIL);
+    }
+
+    /**
+     * 校验短信验证码
+     * Author:  zwg
+     * Date:    180903
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function checkVerifyCode()
+    {
+        $this->validate(request(), [
+            'verify_code' => 'required|min:4'
+        ]);
+        $verifyCode = request()->get('verify_code');
+        $user = request()->get('current_user');
+        $result = SmsService::checkVerifyCode($user->mobile, $verifyCode);
+        if($result) {
+            $token = str_random();
+            Cache::put('user_pay_password_modify_temp_token_' . $user->id, $token, 3);
+            return Result::success([
+                'temp_token' => $token
+            ]);
+        }
+        throw new ParamInvalidException('验证码错误');
+    }
 
     /**
      * 获取用户origin_id
