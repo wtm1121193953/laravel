08aa57cdbdc24f2a38b8ec86a60300f1a6214a7b
Evan <574583188@qq.com>
2018-09-03 11:35:01 +0800

    修改平台微信支付配置

--- config/platform.php
+++ config/platform.php
@@ -6,9 +6,9 @@ return [
         'app_secret' => env('APP_ENV') == 'production' ? '5e2648fc79ccd467f6abadb3347d813e' : '8f5fed3b1edfa44d409416db60f22bfb',
     ],
     'wechat_pay' => [
-        'mch_id' => '',
-        'key' => '',
-        'cert_path' => '',
-        'key_path' => '',
+        'mch_id' => '1513745891',
+        'key' => '57k3434rgtk0g4w4ey45guerdudkjty3',
+        'cert_path' => storage_path('app/wxPayCert/platform/apiclient_cert.pem'),
+        'key_path' => storage_path('app/wxPayCert/platform/apiclient_key.pem'),
     ]
 ];
