22ea8c9d5c5575aa175d7d620ded58515ff4e520
zwg <wenguang.zhang@shoptps.com>
2018-09-05 17:42:53 +0800

    添加下单相关接口

--- routes/api/user_app.php
+++ routes/api/user_app.php
@@ -28,6 +28,11 @@ Route::prefix('app/user')
         Route::get('merchant/categories/tree', 'MerchantCategoryController@getTree');
         Route::get('merchants', 'MerchantController@getList');
         Route::get('merchant/detail', 'MerchantController@detail');
+        Route::get('merchant/getDishesCategoryAndGoods','DishesController@getDishesCategory');
+        Route::get('merchant/getHotDishesGoods','DishesController@getHotDishesGoods');
+        Route::post('merchant/dishesOrder','DishesController@add')->middleware(UserLoginFilter::class);
+        Route::get('merchant/dishesDetail','DishesController@detail')->middleware(UserLoginFilter::class);
+
 
         Route::get('goods', 'GoodsController@getList');
         Route::get('goods/detail', 'GoodsController@detail');
@@ -78,7 +83,7 @@ Route::prefix('app/user')
         Route::post('bankcard/addCard', 'BankCardsController@addCard')->middleware(UserLoginFilter::class);;
         Route::post('bankcard/changDefault', 'BankCardsController@changDefault')->middleware(UserLoginFilter::class);;
         Route::post('bankcard/deleteCard', 'BankCardsController@delCard')->middleware(UserLoginFilter::class);;
-        Route::post('bankcard/getCardsList', 'BankCardsController@getCardsList')->middleware(UserLoginFilter::class);;
+        Route::get('bankcard/getCardsList', 'BankCardsController@getCardsList')->middleware(UserLoginFilter::class);;
 
         //Tps绑定接口
         Route::get('tps/getBindInfo', 'TpsBindController@getBindInfo')->middleware(UserLoginFilter::class);;
