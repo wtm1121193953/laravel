ea5832d17502da9eec2345711c097789c3a78235
zwg <wenguang.zhang@shoptps.com>
2018-09-11 20:25:20 +0800

    修改app订单列表及订单详情接口

--- app/Http/Controllers/UserApp/OrderController.php
+++ app/Http/Controllers/UserApp/OrderController.php
@@ -15,18 +15,24 @@ use App\Exceptions\ParamInvalidException;
 use App\Http\Controllers\Controller;
 use App\Modules\Dishes\DishesGoods;
 use App\Modules\Dishes\DishesItem;
+use App\Modules\FeeSplitting\FeeSplittingService;
 use App\Modules\Goods\Goods;
+use App\Modules\Goods\GoodsService;
 use App\Modules\Merchant\Merchant;
+use App\Modules\Merchant\MerchantService;
 use App\Modules\Order\Order;
 use App\Modules\Order\OrderItem;
 use App\Modules\Order\OrderPay;
 use App\Modules\Order\OrderRefund;
 use App\Modules\Order\OrderService;
+use App\Modules\Setting\SettingService;
 use App\Modules\User\User;
 use App\Modules\UserCredit\UserCreditRecord;
 use App\Modules\Wechat\WechatService;
 use App\Result;
 use App\Support\Alipay;
+use App\Support\Utils;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Log;
 use App\Modules\Dishes\Dishes;
 use App\Modules\Merchant\MerchantSettingService;
@@ -40,36 +46,88 @@ class OrderController extends Controller
         $status = request('status');
         $user = request()->get('current_user');
 
-        $data = OrderService::getList([
-            'userId' => $user->id,
-            'status' => $status
-        ]);
+        $merchantShareInMiniprogram = SettingService::getValueByKey('merchant_share_in_miniprogram');
 
+        $currentOperId = request()->get('current_oper_id');
+        $data = Order::where('user_id', $user->id)
+            ->where(function (Builder $query) {
+                $query->where('type', Order::TYPE_GROUP_BUY)
+                    ->orWhere(function (Builder $query) {
+                        $query->where('type', Order::TYPE_SCAN_QRCODE_PAY)
+                            ->whereIn('status', [4, 6, 7]);
+                    })->orWhere('type', Order::TYPE_DISHES);
+            })
+            ->when($merchantShareInMiniprogram != 1, function (Builder $query) use ($currentOperId) {
+                $query->where('oper_id', $currentOperId);
+            })
+            ->when($status, function (Builder $query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->orderByDesc('id')
+            ->paginate();
+        $data->each(function ($item) use ($currentOperId) {
+            $item->items = OrderItem::where('order_id', $item->id)->get();
+            // 判断商户是否是当前小程序关联运营中心下的商户
+            $item->isOperSelf = $item->oper_id === $currentOperId ? 1 : 0;
+            $item->goods_end_date = Goods::withTrashed()->where('id', $item->goods_id)->value('end_date');
+            $item->merchant_logo = Merchant::where('id', $item->merchant_id)->value('logo');
+            $item->signboard_name = Merchant::where('id', $item->merchant_id)->value('signboard_name');
+            if ($item->type == Order::TYPE_DISHES) {
+                $item->dishes_items = DishesItem::where('dishes_id', $item->dishes_id)->get();
+            }
+        });
         return Result::success([
             'list' => $data->items(),
             'total' => $data->total(),
         ]);
     }
 
-    public function detail(){
+    public function detail()
+    {
         $this->validate(request(), [
             'order_no' => 'required'
         ]);
-        $user = request()->get('current_user');
-        $detail = Order::where('order_no', request('order_no'))
-            ->where('user_id', $user->id)
-            ->firstOrFail();
-        $detail->items = OrderItem::where('order_id', $detail->id)->get();
-
+        $detail = Order::where('order_no', request('order_no'))->firstOrFail();
+        // 只返回一个核销码
+        $orderItem = OrderItem::where('order_id', $detail->id)->first();
+        $detail->items = !empty($orderItem) ? [$orderItem] : [];
+        $currentOperId = request()->get('current_oper_id');
+        // 判断商户是否是当前小程序关联运营中心下的商户
+        if ($detail->pay_target_type == Order::PAY_TARGET_TYPE_PLATFORM) {
+            // 如果是需要支付到平台的订单
+            if (!$currentOperId) { // 如果当前operId是0, 表示是在平台的小程序内
+                $detail->isOperSelf = 1;
+            } else {
+                $detail->isOperSelf = 0;
+            }
+        } else {
+            $detail->isOperSelf = $detail->oper_id === $currentOperId ? 1 : 0;
+        }
+        $merchant_of_order = MerchantService::getById($detail->merchant_id);
+        $detail->signboard_name = $merchant_of_order->value('signboard_name');
+        // 积分记录
         $creditRecord = UserCreditRecord::where('order_no', $detail->order_no)
             ->where('type', 1)
-            ->where('user_id', $user->id)
             ->first();
-        if (!empty($creditRecord)){
+        if (!empty($creditRecord)) {
             $detail->user_level = $creditRecord->user_level;
             $detail->user_level_text = User::getLevelText($creditRecord->user_level);
             $detail->credit = $creditRecord->credit;
         }
+        // 单品订单
+        if ($detail->type == Order::TYPE_DISHES) {
+            $detail->dishes_items = DishesItem::where('dishes_id', $detail->dishes_id)->get();
+        }
+        // 查看分润详情
+        $userFeeSplittingRatioToSelf = FeeSplittingService::getUserFeeSplittingRatioToSelfByMerchantId($detail->merchant_id);
+        $detail->fee_splitting_amount = Utils::getDecimalByNotRounding($detail->pay_price * $userFeeSplittingRatioToSelf, 2);
+
+        // 贡献值
+        $detail->consume_quota = floor($detail->pay_price);
+
+        //商家详情
+        $detail['merchant'] = $merchant_of_order;
+        $detail['good'] = GoodsService::getById($detail->goods_id);
         return Result::success($detail);
     }
 
@@ -131,7 +189,7 @@ class OrderController extends Controller
         $user = request()->get('current_user');
         $merchant = Merchant::findOrFail($dishes->merchant_id);
         $currentOperId = request()->get('current_oper_id');
-        
+
         if ($userIdByDish != $user->id) {
             throw new ParamInvalidException('参数错误');
         }
@@ -171,7 +229,7 @@ class OrderController extends Controller
         $order->settlement_rate = $merchant->settlement_rate;
         $order->remark = request('remark', '');
         $order->pay_target_type = $merchant_oper->pay_to_platform ? Order::PAY_TARGET_TYPE_PLATFORM : Order::PAY_TARGET_TYPE_OPER;
-        empty($order->open_id)?$order->open_id = 'mock_open_id':$order->open_id=$order->open_id;
+        empty($order->open_id) ? $order->open_id = 'mock_open_id' : $order->open_id = $order->open_id;
         $order->save();
         if ($order->pay_target_type == Order::PAY_TARGET_TYPE_PLATFORM) { // 如果是支付到平台
             if ($currentOperId == 0) { // 在平台小程序下
@@ -229,12 +287,12 @@ class OrderController extends Controller
             'pay_type' => 'required',
         ]);
         $price = request('price');
-        if($price <= 0 ){
+        if ($price <= 0) {
             throw new ParamInvalidException('价格不合法');
         }
         $user = request()->get('current_user');
         $merchant = Merchant::first(request('merchant_id'));
-        if(empty($merchant)){
+        if (empty($merchant)) {
             throw new DataNotFoundException('商户信息不存在！');
         }
 
@@ -262,7 +320,7 @@ class OrderController extends Controller
         $order->pay_target_type = $merchant_oper->pay_to_platform ? Order::PAY_TARGET_TYPE_PLATFORM : Order::PAY_TARGET_TYPE_OPER;
         $order->save();
 
-        if($payType == 1){
+        if ($payType == 1) {
             // 如果是微信支付
             // todo 微信支付暂时跳过
 //            $sdkConfig = $this->_wechatUnifyPay($order);
@@ -272,7 +330,7 @@ class OrderController extends Controller
                 'order_no' => $orderNo,
                 'sdk_config' => $sdkConfig,
             ]);
-        }else {
+        } else {
             // 如果是支付宝支付
             $sdkConfig = Alipay::pay($order);
             return Result::success([
@@ -296,20 +354,20 @@ class OrderController extends Controller
         $orderNo = request('order_no');
         $order = Order::where('order_no', $orderNo)->firstOrFail();
 
-        if($order->status == Order::STATUS_PAID){
+        if ($order->status == Order::STATUS_PAID) {
             throw new ParamInvalidException('该订单已支付');
         }
-        if($order->status == Order::STATUS_CANCEL){
+        if ($order->status == Order::STATUS_CANCEL) {
             throw new ParamInvalidException('该订单已取消');
         }
-        if($order->status != Order::STATUS_UN_PAY){
+        if ($order->status != Order::STATUS_UN_PAY) {
             throw new BaseResponseException('订单状态异常');
         }
 
         $payType = request('pay_type', 1);
         $order->pay_type = $payType;
         $order->save();
-        if($payType == 1){
+        if ($payType == 1) {
             // 如果是微信支付
             // todo 暂时跳过微信支付
 //            $sdkConfig = $this->_wechatUnifyPay($order);
@@ -318,7 +376,7 @@ class OrderController extends Controller
                 'order_no' => $orderNo,
                 'sdk_config' => $sdkConfig,
             ]);
-        }else {
+        } else {
             // 如果是支付宝支付
             $sdkConfig = Alipay::pay($order);
             return Result::success([
@@ -339,7 +397,7 @@ class OrderController extends Controller
         ]);
         $orderNo = request('order_no');
         $order = Order::where('order_no', $orderNo)->firstOrFail();
-        if($order->status != Order::STATUS_PAID){
+        if ($order->status != Order::STATUS_PAID) {
             throw new BaseResponseException('订单状态不允许退款');
         }
         // 查询支付记录
@@ -350,7 +408,7 @@ class OrderController extends Controller
         $orderRefund->order_no = $order->order_no;
         $orderRefund->amount = $orderPay->amount;
         $orderRefund->save();
-        if($order->pay_type == 1){
+        if ($order->pay_type == 1) {
             // todo 暂时跳过支付, 直接返回成功
             // 微信退款成功
             $orderRefund->refund_id = 'mock refund id';
@@ -367,7 +425,7 @@ class OrderController extends Controller
             $result = $payApp->refund->byTransactionId($orderPay->transaction_no, $orderRefund->id, $orderPay->amount * 100, $orderPay->amount * 100, [
                 'refund_desc' => '用户发起退款',
             ]);
-            if($result['return_code'] === 'SUCCESS' && array_get($result, 'result_code') === 'SUCCESS'){
+            if ($result['return_code'] === 'SUCCESS' && array_get($result, 'result_code') === 'SUCCESS') {
                 // 微信退款成功
                 $orderRefund->refund_id = $result['refund_id'];
                 $orderRefund->status = 2;
@@ -377,7 +435,7 @@ class OrderController extends Controller
                 $order->save();
                 $this->decSellNumber($order);
                 return Result::success($orderRefund);
-            }else {
+            } else {
                 Log::error('微信退款失败 :', [
                     'result' => $result,
                     'params' => [
@@ -387,9 +445,9 @@ class OrderController extends Controller
                 ]);
                 throw new BaseResponseException('微信退款失败');
             }
-        }else {
+        } else {
             $result = Alipay::refund($orderPay, $orderRefund);
-            if (!empty($result->code)&& $result->code == 10000){
+            if (!empty($result->code) && $result->code == 10000) {
                 // 支付宝退款成功
                 $orderRefund->refund_id = '';
                 $orderRefund->status = 2;
@@ -399,7 +457,7 @@ class OrderController extends Controller
                 $order->save();
                 $this->decSellNumber($order);
                 return Result::success($orderRefund);
-            }else{
+            } else {
                 Log::error('支付宝退款失败 :', [
                     'result' => $result,
                     'params' => [
@@ -430,9 +488,9 @@ class OrderController extends Controller
             'openid' => $order->open_id,
         ];
         $unifyResult = $payApp->order->unify($data);
-        if($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS'){
+        if ($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS') {
             $order->save();
-        }else {
+        } else {
             Log::error('微信统一下单失败', [
                 'payConfig' => $payApp->getConfig(),
                 'data' => $data,
@@ -464,7 +522,7 @@ class OrderController extends Controller
             'openid' => request()->get('current_open_id'),
         ];
         $unifyResult = $payApp->order->unify($data);
-        if(!($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS')){
+        if (!($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS')) {
             Log::error('微信统一下单失败', [
                 'payConfig' => $payApp->getConfig(),
                 'data' => $data,
@@ -505,9 +563,9 @@ class OrderController extends Controller
             'openid' => request()->get('current_open_id'),
         ];
         $unifyResult = $payApp->order->unify($data);
-        if($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS'){
+        if ($unifyResult['return_code'] === 'SUCCESS' && array_get($unifyResult, 'result_code') === 'SUCCESS') {
             $order->save();
-        }else {
+        } else {
             Log::error('微信统一下单失败', [
                 'payConfig' => $payApp->getConfig(),
                 'data' => $data,
@@ -525,15 +583,15 @@ class OrderController extends Controller
      */
     private function decSellNumber($order)
     {
-        if ($order->type == Order::TYPE_GROUP_BUY){
+        if ($order->type == Order::TYPE_GROUP_BUY) {
             Goods::where('id', $order->goods_id)
                 ->where('merchant_id', $order->merchant_id)
                 ->decrement('sell_number', $order->buy_number);
-        }elseif ($order->type == Order::TYPE_DISHES){
+        } elseif ($order->type == Order::TYPE_DISHES) {
             $dishesItems = DishesItem::where('merchant_id', $order->merchant_id)
                 ->where('dishes_id', $order->dishes_id)
                 ->get();
-            foreach ($dishesItems as $item){
+            foreach ($dishesItems as $item) {
                 DishesGoods::where('id', $item->dishes_goods_id)
                     ->where('merchant_id', $item->merchant_id)
                     ->decrement('sell_number', $item->number);
