ca34c53bdeaa9625fd87937eb98458d50f42f1fc
Evan <574583188@qq.com>
2018-09-18 11:38:11 +0800

    修改更新脚本, 清除分润数据

--- app/Console/Commands/Updates/V1_4_3.php
+++ app/Console/Commands/Updates/V1_4_3.php
@@ -54,20 +54,20 @@ class V1_4_3 extends Command
     {
         // 1. 清楚结算出的分润
         // 代码全部注释掉, 以免后面误操作
-//        Wallet::where('id', '>', 0)->delete();
-//        FeeSplittingRecord::where('id', '>', 0)->delete();
-//        WalletBalanceUnfreezeRecord::where('id', '>', 0)->delete();
-//        WalletBatch::where('id', '>', 0)->delete();
-//        WalletBill::where('id', '>', 0)->delete();
-//        WalletConsumeQuotaRecord::where('id', '>', 0)->delete();
-//        WalletConsumeQuotaUnfreezeRecord::where('id', '>', 0)->delete();
-//        WalletWithdraw::where('id', '>', 0)->delete();
-//        Order::where('id', '>', 0)->update(['splitting_status' => 1, 'splitting_time' => null]);
+        Wallet::where('id', '>', 0)->delete();
+        FeeSplittingRecord::where('id', '>', 0)->delete();
+        WalletBalanceUnfreezeRecord::where('id', '>', 0)->delete();
+        WalletBatch::where('id', '>', 0)->delete();
+        WalletBill::where('id', '>', 0)->delete();
+        WalletConsumeQuotaRecord::where('id', '>', 0)->delete();
+        WalletConsumeQuotaUnfreezeRecord::where('id', '>', 0)->delete();
+        WalletWithdraw::where('id', '>', 0)->delete();
+        Order::where('id', '>', 0)->update(['splitting_status' => 1, 'splitting_time' => null]);
 //
 //        // 删除错误的权限
 //        AdminAuthRule::where('pid', 35)->delete();
 
-        $this->info("\n初始化银行列表 Start");
+       /* $this->info("\n初始化银行列表 Start");
         $banks = [
             '中国工商银行',
             '中国农业银行',
@@ -371,7 +371,7 @@ class V1_4_3 extends Command
             $bank->name = $item;
             $bank->save();
         }
-        $this->info("\n初始化银行列表 End");
+        $this->info("\n初始化银行列表 End");*/
 
     }
 }
