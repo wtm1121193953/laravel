662d3627d3a4c507ee5e1e6076c7f366a049e3b2
franmy <franmy.wang@shoptps.com>
2018-09-20 15:59:35 +0800

    收藏商户功能 https://www.tapd.cn/22216871/s/1011891 --story=1000210 --user=franmy.wang

--- app/Http/Controllers/UserApp/MerchantFollowController.php
+++ app/Http/Controllers/UserApp/MerchantFollowController.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers\UserApp;
 
 
+use App\Exceptions\BaseResponseException;
 use App\Http\Controllers\Controller;
 use App\Modules\Merchant\MerchantFollowService;
 use App\Modules\Merchant\MerchantService;
@@ -20,11 +21,19 @@ class MerchantFollowController extends Controller
     public function modifyFollowStatus()
     {
         $user = request()->get('current_user');
+        $merchantId = request('merchant_id');
+        $status = request('status');
+        if (!$merchantId) {
+            throw new BaseResponseException('商户ID不能为空');
+        }
+        if (!$status) {
+            throw new BaseResponseException('状态不能为空');
+        }
 
         $data = MerchantFollowService::modifyFollows([
-            'status' => request('status'), //1-未关注，2-已关注
+            'status' => $status, //1-未关注，2-已关注
             'user_id' => $user->id,
-            'merchant_id' => request('merchant_id')
+            'merchant_id' => $merchantId
         ]);
 
         return Result::success([
