ed62a91749018e46e8a409182ab54ac5b2e8edea
tangwenqiang <tim.tang@shoptps.com>
2018-09-20 16:36:18 +0800

    运营中心报表

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -10,13 +10,46 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Modules\Oper\OperStatisticsService;
 use App\Result;
+use Illuminate\Support\Carbon;
 
 class StatisticsController extends Controller
 {
     public function oper()
     {
-        $startDate = request('startDate');
-        $endDate = request('endDate');
+        $timeType = request('timeType');
+        switch ($timeType) {
+            case 'all':
+                $startDate = null;
+                $endDate = null;
+                break;
+            case 'today':
+                $startDate = Carbon::now()->startOfDay();
+                $endDate = Carbon::now()->endOfDay();
+                break;
+            case 'yesterday':
+                $startDate = Carbon::now()->subDay()->startOfDay();
+                $endDate = $startDate->copy()->endOfDay();
+                break;
+            case 'month':
+                $startDate = Carbon::now()->startOfMonth();
+                $endDate = Carbon::now()->endOfMonth();
+                break;
+            case 'lastMonth':
+                $startDate = Carbon::now()->subMonth()->startOfMonth();
+                $endDate = $startDate->copy()->endOfMonth();
+                break;
+            default:
+                $startDate = request('startDate');
+                $endDate = request('endDate');
+                break;
+        }
+
+        if($startDate && $startDate instanceof Carbon){
+            $startDate = $startDate->format('Y-m-d');
+        }
+        if($endDate && $endDate instanceof Carbon){
+            $endDate = $endDate->format('Y-m-d');
+        }
 
         $data = OperStatisticsService::getList([
             'startDate' => $startDate,
