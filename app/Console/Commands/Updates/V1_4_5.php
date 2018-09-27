88222c45802ad16fe8cfe8dfb2949dbed1a5ea6d
tangwenqiang <tim.tang@shoptps.com>
2018-09-27 12:16:27 +0800

    运营中心数据统计

--- app/Console/Commands/Updates/V1_4_5.php
+++ app/Console/Commands/Updates/V1_4_5.php
@@ -45,14 +45,17 @@ class V1_4_5 extends Command
     public function handle()
     {
 
-        /*************统计运营中心历史数据start*************/
-        $day = 100;
+        /*************统计运营中心5月份之后历史运营数据start*************/
+        $day = 200;
 
         for($i=1; $i<$day; $i++) {
             $endTime = date('Y-m-d',strtotime("-{$i} day")) . ' 23:59:59';
             OperStatisticsDailyJob::dispatch($endTime);
+            if(date('Y-m-d',strtotime("-{$i} day"))<='2018-05-01') {
+                break;
+            }
         }
-        /*************统计运营中心历史数据end*************/
+        /*************统计运营中心5月份之后历史运营数据end*************/
 
         dd('ok');
     }
