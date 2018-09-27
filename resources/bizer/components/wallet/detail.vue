f4e026972d60db29a4db0ef792504696492dbcf5
liwen <wen.li@shoptps.com>
2018-09-27 12:16:56 +0800

    【业务员后台】财务总... https://www.tapd.cn/22216871/s/1013477 --bug=1001611 --user=李文

--- resources/bizer/components/wallet/detail.vue
+++ resources/bizer/components/wallet/detail.vue
@@ -69,7 +69,7 @@
         <el-col :gutter="20" v-if="billData.type == 7 || billData.type == 8">
             <el-form label-width="120px" label-position="left" size="small">
                 <el-col :span="12">
-                    <el-form-item label="订单类型">
+                    <!--<el-form-item label="订单类型">
                         <span v-if="billData.type == 7">
                         提现
                         <span v-if="orderOrWithdrawData.status == 1">[审核中]</span>
@@ -81,6 +81,9 @@
                     </span>
                         <span v-else-if="billData.type == 8">提现失败</span>
                         <span v-else>未知 ({{billData.type}})</span>
+                    </el-form-item>-->
+                    <el-form-item label="交易编号">
+                        {{billData.bill_no}}
                     </el-form-item>
                     <el-form-item label="提现金额">
                         {{orderOrWithdrawData.amount}}元
@@ -88,19 +91,30 @@
                     <el-form-item label="到账金额">
                         {{orderOrWithdrawData.remit_amount}}元
                     </el-form-item>
+                    <el-form-item label="手续费">
+                        {{orderOrWithdrawData.charge_amount}}
+                    </el-form-item>
+                    <!--<el-form-item label="运营中心名称">
+                        {{billData.oper_name}}
+                    </el-form-item>-->
+                </el-col>
+                <el-col :span="12">
+                    <el-form-item label="提现发起时间">
+                        {{orderOrWithdrawData.created_at}}
+                    </el-form-item>
+                    <!--<el-form-item label="余额">
+                        {{billData.after_amount}}
+                    </el-form-item>-->
                     <el-form-item label="提现账户名">
                         {{orderOrWithdrawData.bank_card_open_name}}
+                    </el-form-item>
+                    <el-form-item label="提现银行卡">
+                        {{orderOrWithdrawData.bank_card_no}}
                     </el-form-item>
                     <el-form-item label="开户行">
                         {{orderOrWithdrawData.bank_name}}
                     </el-form-item>
-                    <el-form-item label="运营中心名称">
-                        {{billData.oper_name}}
-                    </el-form-item>
-                    <!--<el-form-item label="快递编号">
-                        {{orderOrWithdrawData.invoice_express_no}}
-                    </el-form-item>-->
-                    <el-form-item label="交易状态">
+                    <el-form-item label="提现状态">
                         <span v-if="orderOrWithdrawData.status == 1">审核中</span>
                         <span v-else-if="orderOrWithdrawData.status == 2">审核通过</span>
                         <span v-else-if="orderOrWithdrawData.status == 3">已打款</span>
@@ -108,32 +122,12 @@
                         <span v-else-if="orderOrWithdrawData.status == 5">审核不通过</span>
                         <span v-else>未知 ({{orderOrWithdrawData.status}})</span>
                     </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="交易编号">
-                        {{billData.bill_no}}
-                    </el-form-item>
-                    <el-form-item label="手续费">
-                        {{orderOrWithdrawData.charge_amount}}
-                    </el-form-item>
-                    <el-form-item label="余额">
-                        {{billData.after_amount}}
-                    </el-form-item>
-                    <el-form-item label="提现银行卡">
-                        {{orderOrWithdrawData.bank_card_no}}
-                    </el-form-item>
-                    <el-form-item label="提现发起时间">
-                        {{orderOrWithdrawData.created_at}}
-                    </el-form-item>
-                    <!--<el-form-item label="发票快递公司">
-                        {{orderOrWithdrawData.invoice_express_company}}
-                    </el-form-item>-->
-                    <el-form-item label="提现编号">
+                    <!--<el-form-item label="提现编号">
                         {{orderOrWithdrawData.withdraw_no}}
                     </el-form-item>
                     <el-form-item label="备注">
                         {{orderOrWithdrawData.remark}}
-                    </el-form-item>
+                    </el-form-item>-->
                 </el-col>
                 <el-col class="tips">注意：具体到账时间请以银行通知时间为准</el-col>
             </el-form>
