655b9f821c035f8aca6b1c5072372832a22814b1
JerryChan <jerry.chen@shoptps.com>
2018-09-29 14:27:31 +0800

    数据库图片迁移COS 修改配置方式

--- config/cos.php
+++ config/cos.php
@@ -1,6 +1,11 @@
 <?php
     return [
-        'cos_url'                 =>  'https://daqian-public-1257640953.cos.ap-guangzhou.myqcloud.com/',
-        'default_image_save_path' =>  'image/item',
-        'test_image'              =>  'test/image'
+        'cos_url'                   =>  'https://daqian-public-1257640953.cos.ap-guangzhou.myqcloud.com/',
+        'default_image_save_path'   =>  'image/item',
+        'test_image'                =>  'test/image',
+        'region'                    =>  'ap-guangzhou',
+        'timeout'                   =>  60,
+        'connect_timeout'           =>  60,
+        'bucket'                    =>  'daqian-public-1257640953',
+        'scheme'                    =>  'https'
     ];
--- config/filesystems.php
+++ config/filesystems.php
@@ -66,17 +66,17 @@ return [
 
         'cosv5' => [
             'driver' => 'cosv5',
-            'region'          => env('COSV5_REGION', 'ap-guangzhou'),
+            'region'          => config('cos.region', 'ap-guangzhou'),
             'credentials'     => [
-                'appId'     => env('COSV5_APP_ID'),
-                'secretId'  => env('COSV5_SECRET_ID'),
-                'secretKey' => env('COSV5_SECRET_KEY'),
+                'appId'     => env('COSV5_APP_ID', '1257640953'),
+                'secretId'  => env('COSV5_SECRET_ID', 'AKIDreHl4Gu6ohZUEoFPSVO8h5HSRIyTl132'),
+                'secretKey' => env('COSV5_SECRET_KEY', 'yqZ7UgjbIi0CE7rYKn8HF1mOo372mLn7'),
             ],
-            'timeout'         => env('COSV5_TIMEOUT', 60),
-            'connect_timeout' => env('COSV5_CONNECT_TIMEOUT', 60),
-            'bucket'          => env('COSV5_BUCKET'),
-            'cdn'             => env('COSV5_CDN'),
-            'scheme'          => env('COSV5_SCHEME', 'https'),
+            'timeout'         => config('cos.timeout', 60),
+            'connect_timeout' => config('cos.connect_timeout', 60),
+            'bucket'          => config('cos.bucket', 'daqian-public-1257640953'),
+            'cdn'             => config('cos.cos_url'),
+            'scheme'          => config('cos.scheme', 'https'),
         ],
 
     ],
