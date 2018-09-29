b6b8ed40f3eb0fc3050086676e37a8bb7555c1f4
JerryChan <jerry.chen@shoptps.com>
2018-09-29 14:56:12 +0800

    修改配置文件

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,17 +66,17 @@ return [
 
         'cosv5' => [
             'driver' => 'cosv5',
-            'region'          => config('cos.region', 'ap-guangzhou'),
+            'region'          => 'ap-guangzhou',
             'credentials'     => [
                 'appId'     => env('COSV5_APP_ID', '1257640953'),
                 'secretId'  => env('COSV5_SECRET_ID', 'AKIDreHl4Gu6ohZUEoFPSVO8h5HSRIyTl132'),
                 'secretKey' => env('COSV5_SECRET_KEY', 'yqZ7UgjbIi0CE7rYKn8HF1mOo372mLn7'),
             ],
-            'timeout'         => config('cos.timeout', 60),
-            'connect_timeout' => config('cos.connect_timeout', 60),
-            'bucket'          => config('cos.bucket', 'daqian-public-1257640953'),
-            'cdn'             => config('cos.cos_url'),
-            'scheme'          => config('cos.scheme', 'https'),
+            'timeout'         => 60,
+            'connect_timeout' => 60,
+            'bucket'          => env('COSV5_SECRET_KEY', 'daqian-public-1257640953'),
+            'cdn'             => env('COSV5_CDN', '#https://{your-bucket-name}-{your-app-id}.file.myqcloud.com'),
+            'scheme'          => env('COSV5_SCHEME', 'https'),
         ],
 
     ],
