9d786d5d2f1a554d3bdb0da3d35c439ac14ce179
Evan <574583188@qq.com>
2018-09-30 13:54:55 +0800

    去掉单品订单详情中的单价显示

--- resources/bizer/components/order/list.vue
+++ resources/bizer/components/order/list.vue
@@ -119,7 +119,7 @@
                     <!--<template v-if="detailOption.type== 1">
                         <dd>身份：{{detailOption.merchant_name}}</dd>
                     </template>-->
-                    <template v-if="detailOption.type== 1 || detailOption.type== 3">    
+                    <template v-if="detailOption.type== 1">
                         <dd>单价：{{detailOption.price}}元</dd>
                     </template>
                     <dd>总价：{{detailOption.pay_price}}元</dd>
@@ -135,15 +135,15 @@
                 </dl>
                 <dl>
                     <dd>订单号：{{detailOption.order_no}}</dd>
-                    <template v-if="detailOption.type== 1">
-                        <dd>商品名称：{{detailOption.goods_name}}</dd>
-                    </template>
                     <!--<template v-if="detailOption.type== 1 || detailOption.type== 2">
                         <dd>数量：{{detailOption.buy_number}}</dd>
                     </template>-->
                     <dd>手机号：{{detailOption.notify_mobile}}</dd>
                     <!--<dd>返利积分：20</dd>-->
                     <dd>订单创建时间：{{detailOption.created_at}}</dd>
+                    <template v-if="detailOption.type== 1">
+                        <dd>商品名称：{{detailOption.goods_name}}</dd>
+                    </template>
                     <template v-if="detailOption.type== 3">
                         <dd>
                             <p>商品信息：</p>
