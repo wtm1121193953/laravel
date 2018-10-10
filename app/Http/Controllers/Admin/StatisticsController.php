c154b9922e3a371ef2e394687ba541a3ef12e2b1
Evan <574583188@qq.com>
2018-10-10 11:38:06 +0800

    优化运营中心营销统计任务

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -25,16 +25,10 @@ class StatisticsController extends Controller
         }
         switch ($timeType) {
             case 'all':
-                if ($page == 1) {
-                    OperStatisticsService::statistics();
-                }
                 $startDate = null;
                 $endDate = Carbon::now()->endOfDay();
                 break;
             case 'today'://今天的数据需要实时统计，只有第一页做统计，不然翻页慢
-                if ($page == 1) {
-                    OperStatisticsService::statistics();
-                }
                 $startDate = Carbon::now()->startOfDay();
                 $endDate = Carbon::now()->endOfDay();
                 break;
@@ -43,9 +37,6 @@ class StatisticsController extends Controller
                 $endDate = $startDate->copy()->endOfDay();
                 break;
             case 'month':
-                if ($page == 1) {
-                    OperStatisticsService::statistics();
-                }
                 $startDate = Carbon::now()->startOfMonth();
                 $endDate = Carbon::now()->endOfMonth();
                 break;
