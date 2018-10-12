8e54d97c5edaeb8ecf46655978507ebd1a151502
Evan <574583188@qq.com>
2018-10-12 21:39:48 +0800

    修复分润数据

--- app/Http/Controllers/Admin/FeeSplittingController.php
+++ app/Http/Controllers/Admin/FeeSplittingController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Exceptions\BaseResponseException;
+use App\Exceptions\ParamInvalidException;
 use App\Http\Controllers\Controller;
 use App\Modules\FeeSplitting\FeeSplittingRecord;
 use App\Modules\FeeSplitting\FeeSplittingService;
 use App\Modules\Order\OrderService;
+use App\Modules\Wallet\Wallet;
+use App\Modules\Wallet\WalletBill;
 use App\Modules\Wallet\WalletService;
 use App\Result;
 use Illuminate\Support\Facades\DB;
@@ -59,13 +62,47 @@ class FeeSplittingController extends Controller
         $ratio = FeeSplittingService::getReFeeSplittingRatio($feeSplittingRecord, $order);
         if (!$ratio) throw new BaseResponseException('分润比例为空');
 
+        if(
+            $feeSplittingRecord->order_profit_amount == $profitAmount
+            && $feeSplittingRecord->ratio == $ratio
+            && $feeSplittingRecord->amount == floor($profitAmount * $ratio) / 100
+        ){
+            throw new BaseResponseException('该分润数据正确, 不需要重新分润');
+        }
+
         DB::beginTransaction();
         try {
             // 更新分润记录
             $newFeeSplittingRecord = FeeSplittingService::updateFeeSplittingRecord($feeSplittingRecord, $profitAmount, $ratio);
-            // 更新钱包和流水
+            // 判断钱包需要修复的金额
+            $amount = $newFeeSplittingRecord->amount - $feeSplittingRecord->amount;
             $wallet = WalletService::getWalletInfoByOriginInfo($feeSplittingRecord->origin_id, $feeSplittingRecord->origin_type);
-            WalletService::updateWalletAndWalletBill($wallet, $feeSplittingRecord, $newFeeSplittingRecord);
+            // 钱包流水表 添加钱包流水记录
+            if ($feeSplittingRecord->type == FeeSplittingRecord::TYPE_TO_SELF) {
+                $type = WalletBill::TYPE_SELF;
+            } elseif ($feeSplittingRecord->type == FeeSplittingRecord::TYPE_TO_PARENT) {
+                $type = WalletBill::TYPE_SUBORDINATE;
+            } elseif ($feeSplittingRecord->type == FeeSplittingRecord::TYPE_TO_OPER) {
+                $type = WalletBill::TYPE_OPER;
+            } elseif ($feeSplittingRecord->type == FeeSplittingRecord::TYPE_TO_BIZER) {
+                $type = WalletBill::TYPE_BIZER;
+            } else {
+                throw new ParamInvalidException('分润类型参数错误');
+            }
+            if($amount > 0){
+                if($newFeeSplittingRecord->status == FeeSplittingRecord::STATUS_FREEZE){
+                    WalletService::addFreezeBalance($wallet, $amount, $type, $newFeeSplittingRecord->id);
+                }else {
+                    WalletService::addBalance($wallet, $amount, $type, $newFeeSplittingRecord->id);
+                }
+            }else if($amount < 0) {
+                WalletService::minusBalance($wallet, -$amount, $type, $newFeeSplittingRecord->id);
+            }
+
+//            WalletService::addFreezeBalance()
+            // 更新钱包和流水
+//            $wallet = WalletService::getWalletInfoByOriginInfo($feeSplittingRecord->origin_id, $feeSplittingRecord->origin_type);
+//            WalletService::updateWalletAndWalletBill($wallet, $feeSplittingRecord, $newFeeSplittingRecord);
 
             DB::commit();
             return Result::success($newFeeSplittingRecord);
