5238b47ac1470e50b8b68b17a927c44cca0aa065
tangwenqiang <tim.tang@shoptps.com>
2018-10-14 18:22:32 +0800

    地区数据同步

--- app/Modules/Area/AreaService.php
+++ app/Modules/Area/AreaService.php
@@ -53,17 +53,12 @@ class AreaService
      */
     public static function getCityListByKeyword($name)
     {
-        $specialName = ['澳门'];
-        if (in_array($name, $specialName)) {
-            $list = Area::where('name', 'like', "%$name%")->get();
-        } else {
-            $list = Area::where('path', '<>', 1)
-                ->where(function (Builder $query) use ($name){
-                    $query->where('name', 'like', "%$name%")
-                        ->orWhere('spell', 'like', "$name%")
-                        ->orWhere('letter', 'like', "$name%");
-                })->get();
-        }
+        $list = Area::where('path', '<>', 1)
+            ->where(function (Builder $query) use ($name){
+                $query->where('name', 'like', "%$name%")
+                    ->orWhere('spell', 'like', "$name%")
+                    ->orWhere('letter', 'like', "$name%");
+            })->get();
 
         return $list;
     }
