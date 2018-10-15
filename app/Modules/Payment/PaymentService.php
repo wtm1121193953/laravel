c86908767079e703e75c2fd82ade8efa9c4a3603
tangwenqiang <tim.tang@shoptps.com>
2018-10-15 17:44:44 +0800

    支付方式管理

--- app/Modules/Payment/PaymentService.php
+++ app/Modules/Payment/PaymentService.php
@@ -8,6 +8,8 @@
 namespace App\Modules\Payment;
 
 use App\BaseService;
+use App\Exceptions\ParamInvalidException;
+use test\Mockery\Fixtures\EmptyTestCaseV5;
 
 class PaymentService extends BaseService
 {
@@ -19,5 +21,26 @@ class PaymentService extends BaseService
         $query->orderBy('id','desc');
         $data = $query->paginate();
         return $data;
+    }
+
+
+    public static function getPayment(int $type, int $terminal)
+    {
+        $terminals = [
+            1=>'on_pc',
+            2=>'on_app',
+            3=>'on_miniprogram'
+        ];
+        if (empty($terminals[$terminal])) {
+            throw new ParamInvalidException('参数错误');
+        }
+
+        $payment = Payment::where('status',1)
+            ->where('type',$type)
+            ->where($terminals[$terminal],'1')
+            ->get();
+
+        dd($payment);
+
     }
 }
