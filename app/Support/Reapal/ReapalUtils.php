6202f46c54588e888b39e50295054631bc60590c
franmy <franmy.wang@shoptps.com>
2018-10-17 10:26:40 +0800

    代付测试PASS

--- app/Support/Reapal/ReapalUtils.php
+++ app/Support/Reapal/ReapalUtils.php
@@ -10,93 +10,81 @@ namespace App\Support\Reapal;
 
 class ReapalUtils
 {
-//签名函数
-    function createSign ($paramArr,$apiKey) {
+    //签名函数
+    public function createSign($paramArr, $apiKey)
+    {
+        unset($paramArr['sign']);
         global $appSecret;
         $sign = $appSecret;
         ksort($paramArr);
         foreach ($paramArr as $key => $val) {
             if ($key != '' && $val != '') {
-                $sign .= $key.'='.$val.'&';
+                $sign .= $key . '=' . $val . '&';
             }
         }
 
-        $sign = substr ( $sign,0,(strlen ( $sign )-1));
-        $sign.=$appSecret;
-        $sign = md5($sign.$apiKey);
+        $sign = substr($sign, 0, (strlen($sign) - 1));
+        $sign .= $appSecret;
+        $sign = md5($sign . $apiKey);
         return $sign;
     }
+
     /**
      * 生成一个随机的字符串作为AES密钥
      *
-     * @param number $length
+     * @param int $length
      * @return string
      */
-    function generateAESKey($length=16){
+    public function generateAESKey($length = 16)
+    {
         $baseString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
         $AESKey = '';
         $_len = strlen($baseString);
-        for($i=1;$i<=$length;$i++){
-            $AESKey .= $baseString[rand(0, $_len-1)];
+        for ($i = 1; $i <= $length; $i++) {
+            $AESKey .= $baseString[rand(0, $_len - 1)];
         }
         return $AESKey;
     }
-    /**
-     * 通过AES加密请求数据
-     *
-     * @param array $query
-     * @return string
-     */
-    function AESEncryptRequest($encryptKey,array $query){
 
-        return $this->encrypt(json_encode($query),$encryptKey);
-
-    }
-    /**
-     * 通过AES解密请求数据
-     *
-     * @param array $query
-     * @return string
-     */
-    function AESDecryptResponse($encryptKey,$data){
-        return $this->decrypt($data,$encryptKey);
-
-    }
     /**
      * 通过RSA，使用融宝公钥，加密本次请求的AESKey
      *
+     * @param $key
+     * @param $reapalPublicKey
      * @return string
      */
-    function RSAEncryptkey($encryptKey,$reapalPublicKey){
-        $public_key= $this->getPublicKey($reapalPublicKey);
+    public function encryptKey($key, $reapalPublicKey)
+    {
+        $public_key = file_get_contents($reapalPublicKey);
 
         $pu_key = openssl_pkey_get_public($public_key);//这个函数可用来判断公钥是否是可用的
 
-        openssl_public_encrypt($encryptKey,$encrypted,$pu_key);//公钥加密
+        openssl_public_encrypt($key, $encrypted, $pu_key);//公钥加密
 
         return base64_encode($encrypted);
-
-
-
     }
+
     /**
      * 通过RSA，使用融宝公钥，加密本次请求的AESKey
      *
+     * @param $encryptKey
+     * @param $merchantPrivateKey
      * @return string
      */
-    function decryptKey($encryptKey,$merchantPrivateKey){
-        $private_key= $this->getPrivateKey($merchantPrivateKey);
-        $pi_key =  openssl_pkey_get_private($private_key);//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
-        openssl_private_decrypt(base64_decode($encryptKey),$decrypted,$pi_key);//私钥解密
+    public function decryptKey($encryptKey, $merchantPrivateKey)
+    {
+        $private_key = file_get_contents($merchantPrivateKey);
+        $pi_key = openssl_pkey_get_private($private_key);//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
+        openssl_private_decrypt(base64_decode($encryptKey), $decrypted, $pi_key);//私钥解密
         return $decrypted;
 
     }
 
-    function encrypt($input, $key) {
+    public function encrypt($input, $key) {
         $size = @mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
         $input = $this->pkcs5_pad($input, $size);
         $td = @mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
-        $iv = @mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        $iv = @mcrypt_create_iv (@mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
         @mcrypt_generic_init($td, $key, $iv);
         $data = @mcrypt_generic($td, $input);
         @mcrypt_generic_deinit($td);
@@ -105,11 +93,11 @@ class ReapalUtils
         return $data;
     }
 
-    function pkcs5_pad ($text, $blocksize) {
+    public function pkcs5_pad ($text, $blocksize) {
         $pad = $blocksize - (strlen($text) % $blocksize);
         return $text . str_repeat(chr($pad), $pad);
     }
-    function decrypt($sStr, $sKey) {
+    public function decrypt($sStr, $sKey) {
         $decrypted= @mcrypt_decrypt(
             MCRYPT_RIJNDAEL_128,
             $sKey,
@@ -123,59 +111,73 @@ class ReapalUtils
         return $decrypted;
     }
 
-    function getPrivateKey($cert_path) {
-        $pkcs12 = file_get_contents ( $cert_path );
+    /*public function encrypt($data, $key)
+    {
+        $data = openssl_encrypt($data, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $this->hexToStr($this->hex_iv));
+        $data = base64_encode($data);
+        return $data;
+    }
 
-        return $pkcs12;
-    }
-    function getPublicKey($cert_path) {
-        $pkcs12 = file_get_contents ( $cert_path );
-        return $pkcs12;
-    }
-    function sendHttpRequest($params, $url) {
-        $opts = $this->getRequestParamString ( $params );
+    public function decrypt($data, $key)
+    {
+        $decrypted = openssl_decrypt(base64_decode($data), 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $this->hexToStr($this->hex_iv));
+
+        return $decrypted;
+    }*/
+
+    /**
+     * 加密数据
+     * @param $params
+     * @param $url
+     * @return string
+     */
+    public function sendHttpRequest($params, $url)
+    {
+        $opts = $this->getRequestParamString($params);
         //echo $opts;
-        $ch = curl_init ();
-        curl_setopt ( $ch, CURLOPT_URL, $url );
-        curl_setopt ( $ch, CURLOPT_POST, 1 );
-        curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false);//不验证证书
-        curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false);//不验证HOST
-        curl_setopt ( $ch, CURLOPT_SSLVERSION, 1);
-        curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//不验证证书
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);//不验证HOST
+        curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-type:application/x-www-form-urlencoded;charset=UTF-8'
-        ) );
-        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $opts );
+        ));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $opts);
 
         /**
          * 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
          */
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         // 运行cURL，请求网页
-        $html = curl_exec ( $ch );
+        $html = curl_exec($ch);
         // close cURL resource, and free up system resources
-        curl_close ( $ch );
+        curl_close($ch);
         return $html;
     }
 
     /**
      * 组装报文
      *
-     * @param unknown_type $params
+     * @param $params
      * @return string
      */
-    function getRequestParamString($params) {
+    public function getRequestParamString($params)
+    {
         $params_str = '';
-        foreach ( $params as $key => $value ) {
-            $params_str .= ($key . '=' . (!isset ( $value ) ? '' : urlencode( $value )) . '&');
+        foreach ($params as $key => $value) {
+            $params_str .= ($key . '=' . (!isset ($value) ? '' : urlencode($value)) . '&');
         }
-        return substr ( $params_str, 0, strlen ( $params_str ) - 1 );
+        return substr($params_str, 0, strlen($params_str) - 1);
     }
 
 
-    function send($paramArr, $url, $apiKey, $reapalPublicKey, $merchant_id,$version,$sign_type){
+    public function send($paramArr, $url, $apiKey, $reapalPublicKey, $merchant_id, $version = '', $sign_type = '')
+    {
         //生成签名
-        $sign = $this->createSign($paramArr,$apiKey);
+        $sign = $this->createSign($paramArr, $apiKey);
         $paramArr['sign_type'] = $sign_type;
         $paramArr['sign'] = $sign;
         //生成AESkey
@@ -183,11 +185,20 @@ class ReapalUtils
         $request = array();
         $request['merchant_id'] = $merchant_id;
         //加密key
-        $request['encryptkey'] = $this->RSAEncryptkey($generateAESKey,$reapalPublicKey);
+        $request['encryptkey'] = $this->encryptKey($generateAESKey, $reapalPublicKey);
         //加密数据
-        $request['data'] = $this->AESEncryptRequest($generateAESKey,$paramArr);
+        //print_r($paramArr);exit;
+        $request['data'] = $this->encrypt(json_encode($paramArr), $generateAESKey);
         $request['version'] = $version;
-        //echo json_encode($paramArr);
-        return $this->sendHttpRequest($request,$url);
+        return $this->sendHttpRequest($request, $url);
+    }
+
+    public function hexToStr($hex)
+    {
+        $string = '';
+        for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+            $string .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+        }
+        return $string;
     }
 }
