c1ae70e8ca0e0b5fb80d570b3e6054e1fdd27906
tangwenqiang <tim.tang@shoptps.com>
2018-10-31 10:03:37 +0800

    test

--- app/Http/Controllers/UserApp/MerchantController.php
+++ app/Http/Controllers/UserApp/MerchantController.php
@@ -51,18 +51,18 @@ class MerchantController extends Controller
             $data = ['list' => $list, 'total' => 2];
         }
 
-        $list = $data['list'];
-        $total = $data['total'];
-        foreach ($list as $k=>$v) {
-            $escape_list = ['test','测试','beta'];
-            foreach ($escape_list as $word) {
-                if (strpos($v->name,$word) !== false) {
-                    unset($list[$k]);
-                    $total--;
-                }
-            }
-        }
-        $data = ['list' => $list, 'total' => $total];
+//        $list = $data['list'];
+//        $total = $data['total'];
+//        foreach ($list as $k=>$v) {
+//            $escape_list = ['test','测试','beta'];
+//            foreach ($escape_list as $word) {
+//                if (strpos($v->name,$word) !== false) {
+//                    unset($list[$k]);
+//                    $total--;
+//                }
+//            }
+//        }
+//        $data = ['list' => $list, 'total' => $total];
         return Result::success($data);
     }
 
