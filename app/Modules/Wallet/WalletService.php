16196fd7ddef7684eac7884e94689d0c5c9dc3d7
zwg <wenguang.zhang@shoptps.com>
2018-11-05 18:09:10 +0800

    修改退款时获取订单详情

--- app/Modules/Wallet/WalletService.php
+++ app/Modules/Wallet/WalletService.php
@@ -379,15 +379,10 @@ class WalletService extends BaseService
                 } else {
                     $item->status = 0;
                 }
-                if($item->type==WalletBill::TYPE_PLATFORM_SHOPPING){
+                if($item->type==WalletBill::TYPE_PLATFORM_SHOPPING || $item->type==WalletBill::TYPE_PLATFORM_REFUND){
                     // 如果使用钱包平台消费
                     $order = OrderService::getById($item->obj_id);
                     $item->order = $order;
-                }
-                if($item->type==WalletBill::TYPE_PLATFORM_REFUND){
-                    // 如果使用钱包平台退款
-                    $refundOrder = OrderRefund::where('id',$item->obj_id)->get();
-                    $item->order = $refundOrder;
                 }
             });
 
