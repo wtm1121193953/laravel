c1bb76e8db8e8b0668ca0ceac384f3578387e66c
Evan <574583188@qq.com>
2018-11-07 19:35:34 +0800

    修改旧的结算逻辑, 添加数据修复

--- app/Console/Commands/RepairSchedule20181107.php
+++ app/Console/Commands/RepairSchedule20181107.php
@@ -2,11 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\ConsumeQuotaUnfreezeJob;
+use App\Jobs\FeeSplittingUnfreezeJob;
 use App\Jobs\Schedule\PlatformTradeRecordsDailyJob;
 use App\Jobs\Schedule\SettlementDaily;
 use App\Jobs\Schedule\SettlementWeekly;
 use App\Jobs\SettlementForMerchant;
 use App\Modules\Merchant\Merchant;
+use App\Modules\Order\Order;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
 
@@ -48,7 +51,7 @@ class RepairSchedule20181107 extends Command
 //        PlatformTradeRecordsDailyJob::dispatch(Carbon::createFromFormat('Y-m-d', '2018-11-07')->subDay()->endOfDay()->format('Y-m-d H:i:s'));
 //        SettlementWeekly::dispatch(Merchant::SETTLE_WEEKLY, Carbon::createFromFormat('Y-m-d', '2018-11-07'));
 
-        $date = Carbon::createFromFormat('Y-m-d', '2018-11-07');
+        /*$date = Carbon::createFromFormat('Y-m-d', '2018-11-07');
         $start = $date->copy()->subWeek()->startOfWeek();
         $end = $date->copy()->subWeek()->endOfWeek();
         Merchant::where('settlement_cycle_type', '<>', 1)
@@ -57,7 +60,12 @@ class RepairSchedule20181107 extends Command
                 $merchants->each(function ($item) use ($start, $end){
                     SettlementForMerchant::dispatch($item->id, $start, $end);
                 });
-            });
+            });*/
+
+
+        $order = Order::where('order_no', 'O20181107085947617449');
+        FeeSplittingUnfreezeJob::dispatch($order)/*->delay(Carbon::now()->addDay(1))*/;
+        ConsumeQuotaUnfreezeJob::dispatch($order)/*->delay(Carbon::now()->addDay(1))*/;
 
     }
 }
