86c5bb0826ec0786951ac96c50ab781b798c71da
tangwenqiang <tim.tang@shoptps.com>
2018-11-09 16:10:45 +0800

    块钱批次查询任务

---
+++ app/Jobs/Schedule/SettlementBatchQuery.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 结算单生成批次
+ */
+namespace App\Jobs\Schedule;
+
+use App\Modules\Settlement\SettlementPlatformKuaiQianBatchService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+
+class SettlementBatchQuery implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        //
+        Log::info('块钱批次查询 :Start');
+
+        SettlementPlatformKuaiQianBatchService::batchQuery();
+
+        Log::info('块钱批次查询 :end');
+    }
+}
