d2cdb2d979784cf1683d15d3c6df8631a208c964
JerryChan <crazychen93@outlook.com>
2018-11-13 18:28:22 +0800

    添加对象条件过滤

--- app/Modules/Message/MessageSystemUserBehaviorRecordService.php
+++ app/Modules/Message/MessageSystemUserBehaviorRecordService.php
@@ -17,6 +17,9 @@ class MessageSystemUserBehaviorRecordService extends Model
         $needSaveIds = $ids;
         if (!empty($record->$type)) {
             $needSaveIds = json_decode($record->$type);
+            if(is_object($ids)){
+                $ids = json_decode($ids,true);
+            }
             foreach ($ids as $k => $v){
                 if(!is_numeric($v) || MessageSystem::where('id',$v)->doesntExist()){
                     // 非數字不保存,不存在的ID不保存
@@ -27,6 +30,7 @@ class MessageSystemUserBehaviorRecordService extends Model
                     array_push($needSaveIds,$v);
                 }
             }
+
         }
         $record->$type = json_encode($needSaveIds);
         $record->save();
