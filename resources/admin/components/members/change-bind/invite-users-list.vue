129f36f8463e0298e3501e6ccb173a0c715b0bcb
wen.li <wen.li@daqian520.com>
2018-11-14 10:54:35 +0800

    admin 换绑列表添加绑定时间

--- resources/admin/components/members/change-bind/invite-users-list.vue
+++ resources/admin/components/members/change-bind/invite-users-list.vue
@@ -2,6 +2,7 @@
     <page :title="title" :breadcrumbs="{渠道换绑: '/member/changBind'}">
         <el-table stripe :data="list" v-loading="tableLoading">
             <el-table-column prop="user.id" label="用户ID"/>
+            <el-table-column prop="created_at" label="绑定时间"/>
             <el-table-column prop="user.created_at" label="注册时间"/>
             <el-table-column prop="user.mobile" label="手机号"/>
         </el-table>
