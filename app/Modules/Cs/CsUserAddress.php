b674bfdcdd785ae864485caa1172f20177c7831b
tangwenqiang <tim.tang@shoptps.com>
2018-11-19 17:57:00 +0800

    大千超市基础表

---
+++ app/Modules/Cs/CsUserAddress.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Cs;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CsUserAddress extends Model
+{
+    //
+}
---
+++ database/migrations/2018_11_19_175253_create_cs_user_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCsUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cs_user_addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->default(0)->comment('用户ID');
+            $table->string('province')->default('')->comment('省份');
+            $table->integer('province_id')->default(0)->comment('省份id');
+            $table->string('city')->default('')->comment('城市');
+            $table->integer('city_id')->default(0)->comment('城市id');
+            $table->string('area')->default('')->comment('县区');
+            $table->integer('area_id')->default(0)->comment('县区id');
+            $table->string('address',600)->default('')->comment('详细地址');
+            $table->tinyInteger('is_default')->default(0)->comment('是否默认地址 1是 0否');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cs_user_addresses');
+    }
+}
