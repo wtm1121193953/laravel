f3c49f318b60f39c804b29920c2a8cb49e600d15
JerryChan <crazychen93@outlook.com>
2018-11-20 16:00:34 +0800

    添加超市每日结算模块

---
+++ database/migrations/2018_11_20_122745_alert_table_settlement_platforms_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlertTableSettlementPlatformsColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settlement_platforms', function (Blueprint $table) {
+            //
+            $table->tinyInteger('object_type')->default(1)->comment('商户类型 1-普通商户 2-超市商户')->after('merchant_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
