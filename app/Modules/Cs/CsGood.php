bf937dd552a2ab1641c177c8aeef64dcb5f732d8
tangwenqiang <tim.tang@shoptps.com>
2018-11-20 22:02:09 +0800

    商品列表

--- app/Modules/Cs/CsGood.php
+++ app/Modules/Cs/CsGood.php
@@ -4,7 +4,35 @@ namespace App\Modules\Cs;
 
 use App\BaseModel;
 
+/**
+ * 超市商品类
+ * Class CsGood
+ * @package App\Modules\Cs
+ * @property integer  id
+ * @property integer cs_merchant_id
+ * @property integer cs_merchant_cat_id_level1
+ * @property integer cs_merchant_cat_id_level2
+ * @property string goods_name
+ * @property float market_price
+ * @property float price
+ * @property integer stock
+ * @property string logo
+ * @property string detail_imgs
+ * @property string summary
+ * @property string certificate1
+ * @property string certificate2
+ * @property string certificate3
+ * @property integer status
+ * @property integer audit_status
+ * @property string audit_suggestion
+ */
 class CsGood extends BaseModel
 {
     //
+    const STATUS_ON = 1; //上架
+    const STATUS_OFF = 2; //下架
+
+    const AUDIT_STATUS_AUDITING = 1; //审核中
+    const AUDIT_STATUS_SUCCESS = 2; //审核通过
+    const AUDIT_STATUS_FAIL = 3; //审核不通过
 }
--- app/Modules/Cs/CsMerchantCategory.php
+++ app/Modules/Cs/CsMerchantCategory.php
@@ -7,4 +7,5 @@ use App\BaseModel;
 class CsMerchantCategory extends BaseModel
 {
     //
+    protected $guarded = [];
 }
