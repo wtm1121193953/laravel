a006f789464d26f2ccf7916536d02932f68a1268
Evan <574583188@qq.com>
2018-11-23 18:01:51 +0800

    添加导航表

---
+++ database/migrations/2018_11_23_175159_create_navigations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNavigationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('position')->default('index')->index()->comment('导航位置 index-首页宫格导航');
+            $table->string('title')->default('')->comment('标题');
+            $table->string('icon')->default('')->comment('图标');
+            $table->string('type')->default('merchant_category')->index()->comment('导航类型 cs_index-超市首页, merchant_category-某个类目商户列表, merchant_category_all-商户类目列表, h5-h5链接');
+            $table->string('payload', 1000)->default('')->comment('导航附加数据, json格式字符串');
+            $table->timestamps();
+            $table->comment = 'app导航表';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigations');
+    }
+}
