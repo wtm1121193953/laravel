b107cb28736edd71a4aad520d9d273e1a21c6d08
tangwenqiang <tim.tang@shoptps.com>
2018-11-25 17:30:08 +0800

    超市商户地理位置缓存

--- app/DataCacheService.php
+++ app/DataCacheService.php
@@ -8,6 +8,7 @@
 
 namespace App;
 
+use App\Modules\Cs\CsMerchant;
 use App\Modules\Cs\CsMerchantCategoryService;
 use App\Modules\Cs\CsPlatformCategoryService;
 use App\Modules\Merchant\Merchant;
@@ -17,6 +18,7 @@ use test\Mockery\Fixtures\EmptyTestCaseV5;
 class DataCacheService extends BaseService
 {
     const REDIS_KEY_MERCHANT = 'merchant:id:';//商户信息缓存键值
+    const REDIS_KEY_CS_MERCHANT = 'cs_merchant:id:';//超市商户信息缓存键值
     const REDIS_KEY_PLATFORM_CATS = 'common_data:platform_cats';//平台分类缓存
     const REDIS_KEY_CS_MERCHANT_CATS = 'common_data:merchant_cats:';//商户分类缓存
 
@@ -121,5 +123,36 @@ class DataCacheService extends BaseService
     {
         $cache_key = self::REDIS_KEY_CS_MERCHANT_CATS . '*';
         return Cache::store('redis')->forget($cache_key);
+    }
+
+
+    /**
+     * 获取商户详情缓存
+     * @param $id
+     * @return Merchant|mixed
+     */
+    public static function getCsMerchantDetail($id)
+    {
+        $cache_key = self::REDIS_KEY_CS_MERCHANT . $id;
+        $data = Cache::store('redis')->get($cache_key);
+        if (empty($data)) {
+            $data = CsMerchant::findOrFail($id);
+            Cache::store('redis')->forever($cache_key, $data);
+        }
+        return $data;
+    }
+
+    /**
+     * 删除商户详情缓存
+     * @param array $ids
+     */
+    public static function delCsMerchantDetail($ids = [])
+    {
+        if ($ids) {
+            foreach ($ids as $id) {
+                $cache_key = self::REDIS_KEY_CS_MERCHANT . $id;
+                Cache::store('redis')->forget($cache_key);
+            }
+        }
     }
 }
