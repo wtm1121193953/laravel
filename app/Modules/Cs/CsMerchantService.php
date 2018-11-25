2187f3c165a7f31fb195121aa1da91baf6d87e6b
zenjj <578288786@qq.com>
2018-11-25 14:22:27 +0800

    修改商户没有经纬度时问题

--- app/Modules/Cs/CsMerchantService.php
+++ app/Modules/Cs/CsMerchantService.php
@@ -573,8 +573,6 @@ class CsMerchantService extends BaseService {
      */
     public static function getListAndDistance(array $params = [])
     {
-//        $query = CsMerchant::all();
-//        return $query;
 
         $city_id = array_get($params, 'city_id');
         $lng = array_get($params,'lng');
