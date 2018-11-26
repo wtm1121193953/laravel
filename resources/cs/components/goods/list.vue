12e318e9379d54bef0ee20655e1e0b35d8cd4670
tangwenqiang <tim.tang@shoptps.com>
2018-11-26 17:06:55 +0800

    价格符合

--- resources/cs/components/goods/list.vue
+++ resources/cs/components/goods/list.vue
@@ -44,7 +44,7 @@
 
             <el-table-column prop="id" label="商品ID"/>
             <el-table-column prop="goods_name" label="商品名称"/>
-            <el-table-column prop="price" label="销售价"/>
+            <el-table-column prop="price" label="销售价 ¥"/>
             <el-table-column prop="cs_platform_cat_id_level1_name" label="一级分类"/>
             <el-table-column prop="cs_platform_cat_id_level2_name" label="二级分类"/>
             <el-table-column prop="logo" label="商品图片">
