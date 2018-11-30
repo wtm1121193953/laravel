97a8b2de01bc21e5b9d0dc60998b441127d70934
zenjj <578288786@qq.com>
2018-11-30 10:57:02 +0800

    添加小程序通用配置接口

---
+++ app/Http/Controllers/User/SettingController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim.tang
+ * Date: 2018/11/25/025
+ * Time: 16:36
+ */
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Result;
+
+class SettingController extends Controller
+{
+    /**
+     * app通用配置
+     * @return mixed
+     */
+    public function settings()
+    {
+
+        return Result::success([
+            'supermarket_on' => config('common.supermarket_on'),
+            'index_cs_banner_on' => 1,
+            'index_cs_banner_url' => 'https://daqian-public-1257640953.cos.ap-guangzhou.myqcloud.com/6352c1aecba7dd10fcc87c32bf3d92a1.png'
+        ]);
+    }
+}
