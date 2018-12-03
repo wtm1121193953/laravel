77b14afe83430f1d2668a471cc7cd6edbebf8907
tangwenqiang <tim.tang@daqian520.com>
2018-12-03 10:30:55 +0800

    运营中心入口限制

--- app/Http/Controllers/User/SettingController.php
+++ app/Http/Controllers/User/SettingController.php
@@ -8,6 +8,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Setting\SettingService;
 use App\Result;
 
 class SettingController extends Controller
@@ -20,8 +21,8 @@ class SettingController extends Controller
     {
 
         return Result::success([
-            'supermarket_on' => config('common.supermarket_on'),
-            'index_cs_banner_on' => 1,
+            'supermarket_on' => SettingService::getValueByKey('supermarket_on'),
+            'index_cs_banner_on' => SettingService::getValueByKey('supermarket_index_cs_banner_on'),
             'index_cs_banner_url' => 'https://daqian-public-1257640953.cos.ap-guangzhou.myqcloud.com/6352c1aecba7dd10fcc87c32bf3d92a1.png'
         ]);
     }
