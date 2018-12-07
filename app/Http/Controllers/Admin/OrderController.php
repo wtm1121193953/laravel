0ba14f36d61aa3903d3c48288dcb1cb421966848
wen.li <wen.li@daqian520.com>
2018-12-07 10:57:19 +0800

    交易记录导出bug

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -170,6 +170,7 @@ class OrderController extends Controller
             'startTime' => request('startTime'),
             'endTime' => request('endTime'),
             'user_id' => request('user_id'),
+            'merchant_type' => request('merchant_type'),
         ];
 
         $data = PlatformTradeRecordService::getList($params,true);
